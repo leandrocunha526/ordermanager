a933711ec326106bea6b1e7753f96f19876b7e90
leandrocunha526 <leandrocunha016@gmail.com>
2021-08-09 13:33:58 -0300

    Prepare to deploy

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "./auth";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "https://ordermanager-api.herokuapp.com",
 });
 
 api.interceptors.request.use(async (config) => {
