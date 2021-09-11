57c32c8629133bf8320fbc7f0453651a7357f491
Leandro Cunha <leandrocunha016@gmail.com>
2021-09-11 03:44:47 -0300

    Fix url font awesome and fix components routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,50 @@
-import Routes from "./routes";
+import React, { Fragment } from "react";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { isAuthenticated } from "./services/auth";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import NotFound from "./pages/NotFound";
+import Navbar from "./components/navbar/Navbar";
+import Sidebar from "./components/sidebar/Sidebar";
 import "./App.css";
 
-const App = () => {
-  return <Routes />;
-};
+const NavRoute = ({ exact, path, component: Component }) => (
+  <Route
+    exact={exact}
+    path={path}
+    render={(props) =>
+      isAuthenticated() ? (
+        <div className="container">
+          <Component {...props} />
+          <Navbar />
+          <Sidebar />
+        </div>
+      ) : (
+        <Redirect to={{ pathname: "/", state: { from: props.location } }} />
+      )
+    }
+  />
+);
 
-export default App;
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <Fragment>
+            <Switch>
+              <NavRoute
+                exact
+                path="/dashboard"
+                component={() => <h1>Olá</h1>}
+              />
+              <Route exact path="/" component={SignIn} />
+              <Route exact path="/signup" component={SignUp} />
+              <Route component={NotFound} />
+            </Switch>
+          </Fragment>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
