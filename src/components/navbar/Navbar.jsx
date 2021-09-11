3a8b972ea10c36c369d0ccca67b88dfd58d868d5
Leandro Cunha <leandrocunha016@gmail.com>
2021-09-11 02:58:51 -0300

    Add nav bar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,6 +5,14 @@ import avatar from "../../assets/avatar.svg";
 const Navbar = () => {
   return (
     <nav className="navbar">
+      <div className="nav_icon">
+        <i className="fa fa-bars"></i>
+      </div>
+      <div className="navbar__left">
+        <a href="/" className="active_link">
+          Admin
+        </a>
+      </div>
       <div className="navbar__right">
         <a href="/#">
           <img width="30" src={avatar} alt="avatar" />
