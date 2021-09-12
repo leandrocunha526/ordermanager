d43482f29eeb390aed8faeea5b1870cafff777f5
Leandro Cunha <leandrocunha016@gmail.com>
2021-09-12 02:32:17 -0300

    Import navbar changes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import "./Navbar.css";
 import avatar from "../../assets/avatar.svg";
 
-const Navbar = () => {
+const Navbar = ({ sidebarOpen, openSidebar }) => {
   return (
     <nav className="navbar">
-      <div className="nav_icon">
+      <div className="nav_icon" onClick={() => openSidebar()}>
         <i className="fa fa-bars"></i>
       </div>
       <div className="navbar__left">
@@ -17,7 +17,12 @@ const Navbar = () => {
         <a href="/">Usuários cadastrados</a>
       </div>
       <div className="navbar__right">
-        <img width="30" src={avatar} alt="avatar" />
+        <a href="/#">
+          <i className="fa fa-search"></i>
+        </a>
+        <a href="/#">
+          <img width="30" src={avatar} alt="avatar" />
+        </a>
       </div>
     </nav>
   );
