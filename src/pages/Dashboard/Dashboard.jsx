7c4f6b381dc728a8a3a83ea3dbd167780d286bf3
Leandro Cunha <leandrocunha016@gmail.com>
2021-09-14 21:48:04 -0300

    Import cards to charts

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -13,6 +13,59 @@ const Dashboard = () => {
             <p>Seja bem vindo ao seu dashboard</p>
           </div>
         </div>
+        <div className="charts">
+          <div className="charts__left">
+            <div className="charts__left__title">
+              <div>
+                <h1>Daily Orders</h1>
+              </div>
+              <i className="fa fa-usd"></i>
+            </div>
+          </div>
+          <div className="charts__right">
+            <div className="charts__right__title">
+              <div>
+                <h1>Registers</h1>
+              </div>
+              <i className="fa fa-usd"></i>
+            </div>
+
+            <div className="charts__right__cards">
+              <div className="card1">
+                <h1>Fornecedores</h1>
+                <p></p>
+              </div>
+              <div className="card2">
+                <h1>Insumos</h1>
+                <p></p>
+              </div>
+              <div className="card3">
+                <h1>Users</h1>
+                <p></p>
+              </div>
+              <div className="card4">
+                <h1>Funcionários</h1>
+                <p></p>
+              </div>
+              <div className="card5">
+                <h1>Ordens de serviço</h1>
+                <p></p>
+              </div>
+              <div className="card6">
+                <h1>Máquinas</h1>
+                <p></p>
+              </div>
+              <div className="card7">
+                <h1>Marcas</h1>
+                <p></p>
+              </div>
+              <div className="card8">
+                <h1>Modelos</h1>
+                <p></p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </dashboard>
   );
