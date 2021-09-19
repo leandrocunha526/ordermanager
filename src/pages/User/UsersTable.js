650aa866659087c080a0a206580a9f028741b3e1
Leandro Cunha <leandrocunha016@gmail.com>
2021-09-19 14:20:11 -0300

    Set function anonymous

--- src/pages/User/UsersTable.js
+++ src/pages/User/UsersTable.js
@@ -1,11 +1,12 @@
 import React, {Component} from "react";
 import api from "../../services/api";
-import {withRouter} from "react-router-dom";
+import {withRouter, Link} from "react-router-dom";
 import "./styles/UserTable.css"
 
 class UsersTable extends Component {
     state = {
         users: [],
+        error: []
     };
     componentDidMount(){
         api.get("/user/list")
@@ -17,19 +18,26 @@ class UsersTable extends Component {
             console.log(error);
         })
     }
-    delete(id){
-        try{
-            api.delete(`/user/${id}`);
-        } catch (error){
-            console.log(error);
+    async delete(id){
+        try {
+            await api.delete(`/user/${id}`);
+            alert("Usuário excluído com sucesso você será redirecionado para o dashboard")
+            this.props.history.push("/dashboard");
+        }catch(err){
+            this.setState({
+            error: "Não foi possível excluir o usuário",
+        });
         }
     }
     render(){
         return (
             <main>
             <div className="usertable__container">
+                <div>
                 <h1>Usuários cadastrados</h1>
+                </div>
                     <table>
+                    {this.state.error && <p>{this.state.error}</p>}
                     <thead>
                         <tr>
                             <th>Id</th>
@@ -45,8 +53,11 @@ class UsersTable extends Component {
                         <td>{user.id}</td>
                         <td>{user.name}</td>
                         <td>{user.email}</td>
-                        <td><button className="button__warning">Deletar</button></td>
-                        <td><button className="button__primary">Editar</button></td>
+                        <td><button type="button"
+                        className="button__warning"
+                        onClick={() => this.delete(user.id)}
+                        >Deletar</button></td>
+                        <td><Link to={"/user/edit/" + user.id} className="button__primary">Editar</Link></td>
                         </tr>
                   )}
                   </tbody>
