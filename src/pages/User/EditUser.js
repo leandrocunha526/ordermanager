adb9882b599b7bf04f5c8af091052ec7b006b135
Leandro Cunha <leandrocunha016@gmail.com>
2021-09-23 09:56:02 -0300

    Remove dependency

--- src/pages/User/EditUser.js
+++ src/pages/User/EditUser.js
@@ -1,65 +1,36 @@
-import React, { useEffect } from "react";
-import api from "../../services/api";
-import { withRouter, useParams } from "react-router-dom";
+import React from "react";
+import { withRouter } from "react-router-dom";
 import { Container, Form } from "./styles/EditUser";
-import {useForm} from "react-hook-form";
 
 const EditUser = (props) => {
-
-    const {id} = useParams();
-
-    const {register, handleSubmit, setValue} = useForm({});
-
-    useEffect(() => {
-      api.get(`/user/${id}`).then((result) => {
-        setValue("id", result.data.id);
-        setValue("name", result.data.name);
-        setValue("username", result.data.username);
-        setValue("email", result.data.email);
-        setValue("password", result.data.password);
-      });
-    });
-
-    const onSubmit = (data) => {
-      api.put(`/user/${id}`, data).then(() => {
-        props.history.push("/users");
-      }).catch(error => {
-        console.log(error)
-      })
-    }
-
 
     return (
       <main>
         <Container>
-          <Form  onSubmit={handleSubmit(onSubmit)}>
+          <Form>
             <h1>Editar usuário</h1>
             <label>Nome</label>
             <input
               type="text"
               name="name"
-              {...register("name")}
             />
 
            <label>Nome de usuário</label>
             <input
               type="text"
               name="username"
-              {...register("username")}
             />
 
             <label>Email</label>
             <input
               type="email"
               name="email"
-              {...register("email")}
             />
 
             <label>Senha</label>
             <input
               type="password"
               name="password"
-              {...register("password")}
             />
 
             <button type="submit">Enviar</button>
