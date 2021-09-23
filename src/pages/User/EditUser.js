68cf2d42022105ac63f3703543b81a203f622e17
Leandro Cunha <leandrocunha016@gmail.com>
2021-09-22 21:04:03 -0300

    Update edit form

--- src/pages/User/EditUser.js
+++ src/pages/User/EditUser.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-use-before-define */
 import React, { useEffect } from "react";
 import api from "../../services/api";
 import { withRouter, useParams } from "react-router-dom";
