641fda6f03ba169052b3ca689a8fe8f8ad73cb8b
Leandro Cunha <leandrocunha016@gmail.com>
2021-09-25 17:05:43 -0300

    Fix edit user

--- src/pages/User/EditUser.js
+++ src/pages/User/EditUser.js
@@ -1,127 +1,73 @@
-import React, { Component } from "react";
-import { withRouter } from "react-router-dom";
+import React, { useEffect } from "react";
+import api from "../../services/api";
+import { withRouter, useParams } from "react-router-dom";
 import { Container, Form } from "./styles/EditUser";
-import api from "./../../services/api";
+import {useForm} from "react-hook-form";
 
-class EditUser extends Component {
-  constructor(props) {
-    super(props);
+const EditUser = (props) => {
 
-    this.onChangeFirstName = this.onChangeFirstName.bind(this);
-    this.onChangeLastName = this.onChangeLastName.bind(this);
-    this.onChangeUserName = this.onChangeUserName.bind(this);
-    this.onChangePassword = this.onChangePassword.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
+    const {id} = useParams();
 
-    this.state = {
-      user: [],
-      name: "",
-      firstName: "",
-      lastName: "",
-      username: "",
-      password: "",
-      message: "",
-    };
-  }
+    const {register, handleSubmit, setValue} = useForm({});
 
-  componentDidMount() {
-    api.get("/api/users/"+this.props.match.params.id).then(res => {
-      const user = res.data;
-      this.setState({
-       user
+    useEffect(() => {
+      api.get(`/api/users/${id}`).then((result) => {
+        setValue("id", result.data.id);
+        setValue("firstName", result.data.firstName);
+        setValue("lastName", result.data.lastName);
+        setValue("username", result.data.username);
+        setValue("email", result.data.email);
+        setValue("password", result.data.password);
       });
-    }).catch(error => {
-      console.log(error);
-    })
-  }
+    });
 
-  onChangeFirstName(e) {
-    this.setState({
-      name: e.target.value,
-    });
-  }
-  onChangeLastName(e) {
-    this.setState({
-      firstName: e.target.value,
-    });
-  }
-  onChangeUserName(e) {
-    this.setState({
-      username: e.target.value,
-    });
-  }
-  onChangeEmail(e) {
-    this.setState({
-      email: e.target.value,
-    });
-  }
-  onChangePassword(e) {
-    this.setState({
-      password: e.target.value,
-    });
-  }
-
-  onSubmit(e) {
-    e.preventDefault();
-    const data = {
-      name: this.state.name,
-      username: this.state.username,
-      email: this.state.email,
-      password: this.state.password,
-    };
-    api.put("/api/users/"+this.props.match.params.id, data).then(
-      this.setState({
-        message: "Usuário atualizado com sucesso"
+    const onSubmit = (data) => {
+      api.put(`/api/users/${id}`, data).then(() => {
+        props.history.push("/users");
+      }).catch(error => {
+        console.log(error)
       })
-    );
-  }
+    }
 
-  render() {
+
     return (
       <main>
         <Container>
-          <Form onSubmit={this.onSubmit}>
+          <Form  onSubmit={handleSubmit(onSubmit)}>
             <h1>Editar usuário</h1>
-            <h2>{this.state.message}</h2>
-            <label>Nome:</label>
+            <label>Primeiro nome</label>
             <input
               type="text"
-              name="fistName"
-              value={this.state.user.firstName}
-              onChange={this.onChangeFirstName}
+              name="firstName"
+              {...register("firstName")}
             />
 
-            <label>Último nome:</label>
+            <label>Último nome</label>
             <input
               type="text"
               name="lastName"
-              value={this.state.user.lastName}
-              onChange={this.onChangeLastName}
+              {...register("lastName")}
             />
 
-            <label>Nome de usuário:</label>
+           <label>Nome de usuário</label>
             <input
               type="text"
               name="username"
-              value={this.state.user.username}
-              onChange={this.onChangeUserName}
+              {...register("username")}
             />
 
-            <label>Senha:</label>
+            <label>Senha</label>
             <input
               type="password"
               name="password"
-              value={this.state.user.password}
-              onChange={this.onChangePassword}
+              {...register("password")}
             />
 
             <button type="submit">Enviar</button>
-
           </Form>
         </Container>
       </main>
     );
-  }
 }
 
 export default withRouter(EditUser);
