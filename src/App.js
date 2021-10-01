f5b71cfed4d51ffc611207ac01e59ec759411721
Leandro Cunha <leandrocunha016@gmail.com>
2021-10-01 10:32:36 -0300

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, { Fragment, useState } from "react";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import { isAuthenticated } from "./services/auth";
+
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+
 import NotFound from "./pages/NotFound";
+
 import Dashboard from "./pages/Dashboard/Dashboard";
+
 import UsersTable from "./pages/User/UsersTable";
 import EditUser from "./pages/User/EditUser";
+
 import Machine from "./pages/Machine/MachineTable";
+import Manufacturer from "./pages/Manufacturer/Manufacturer";
+import ManufacturerTable from "./pages/Manufacturer/ManufacturerTable";
+import ManufacturerEdit from "./pages/Manufacturer/ManufacturerEdit";
+
+import Employee from "./pages/Employee/Employee";
+import EmployeeTable from "./pages/Employee/EmployeeTable";
+import EmployeeEdit from "./pages/Employee/EmployeeEdit";
 
 import Navbar from "./components/navbar/Navbar";
 import Sidebar from "./components/sidebar/Sidebar";
@@ -50,7 +62,13 @@ const App = () => {
             <PrivateRoute exact path="/dashboard" component={Dashboard} />
             <PrivateRoute exact path="/users" component={UsersTable} />
             <PrivateRoute path="/user/edit/:id" component={EditUser} />
-            <PrivateRoute path="/machine" component={Machine} />
+            <PrivateRoute path="/machine/list" component={Machine} />
+            <PrivateRoute path="/manufacturer/register" component={Manufacturer} />
+            <PrivateRoute path="/manufacturer/list" component={ManufacturerTable} />
+            <PrivateRoute path="/manufacturer/edit/:id" component={ManufacturerEdit} />
+            <PrivateRoute path="/employee/register" component={Employee} />
+            <PrivateRoute path="/employee/list" component={EmployeeTable} />
+            <PrivateRoute path="/employee/edit/:id" component={EmployeeEdit} />
             <Route exact path="/" component={SignIn} />
             <Route exact path="/signup" component={SignUp} />
             <Route component={NotFound} exact path="*" />
