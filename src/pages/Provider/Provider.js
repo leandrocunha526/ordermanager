7d6be51d6f9a077ab7b33a0620e12bdfaf800df5
Leandro Cunha <leandrocunha016@gmail.com>
2021-10-02 00:04:04 -0300

    Update URL promise request

--- src/pages/Provider/Provider.js
+++ src/pages/Provider/Provider.js
@@ -29,7 +29,7 @@ class SignUp extends Component {
       });
     } else {
       try {
-        await api.post("/api/manufacturers/register", {
+        await api.post("/api/providers/register", {
           corporateName,
           cnpj,
           email,
