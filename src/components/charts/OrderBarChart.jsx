82798bd5a33aa4f667ee8676bbec1c6946653342
leandrocunha526 <leandrocunha016@gmail.com>
2021-10-11 22:07:41 -0300

    Add order bar chart

---
+++ src/components/charts/OrderBarChart.jsx
@@ -0,0 +1,110 @@
+import React, { useState, useEffect } from 'react'
+import {Bar} from 'react-chartjs-2'
+import api from "../../services/api";
+
+const OrderBarChart = () => {
+    const [chartData, setChartData]  = useState({});
+
+
+    const Chart = () => {
+        let order_id = [];
+        let order_price = [];
+
+        api.get("/api/orders/list")
+        .then(res => {
+            for(const dataObj of res.data){
+                order_id.push(parseInt(dataObj.salary));
+                order_price.push(parseFloat(dataObj.birthday));
+
+            }
+            setChartData({
+                labels: order_id,
+                datasets: [{
+                                             label: 'Valor da ordem de serviço (R$)',
+                                             data: order_price,
+                                             backgroundColor: [
+                                                 'rgba(255, 99, 132, 0.2)',
+                                                 'rgba(54, 162, 235, 0.2)',
+                                                 'rgba(255, 206, 86, 0.2)',
+                                                 'rgba(75, 192, 192, 0.2)',
+                                                 'rgba(153, 102, 255, 0.2)',
+                                                 'rgba(255, 159, 64, 0.2)',
+                                                 'rgba(255, 99, 132, 0.2)',
+                                                 'rgba(54, 162, 235, 0.2)',
+                                                 'rgba(255, 206, 86, 0.2)',
+                                                 'rgba(75, 192, 192, 0.2)',
+                                                 'rgba(153, 102, 255, 0.2)',
+                                                 'rgba(255, 159, 64, 0.2)',
+                                                 'rgba(255, 99, 132, 0.2)',
+                                                 'rgba(54, 162, 235, 0.2)',
+                                                 'rgba(255, 206, 86, 0.2)',
+                                                 'rgba(75, 192, 192, 0.2)',
+                                                 'rgba(153, 102, 255, 0.2)',
+                                                 'rgba(255, 159, 64, 0.2)',
+                                                 'rgba(255, 99, 132, 0.2)',
+                                                 'rgba(54, 162, 235, 0.2)',
+                                                 'rgba(255, 206, 86, 0.2)',
+                                                 'rgba(75, 192, 192, 0.2)',
+                                                 'rgba(153, 102, 255, 0.2)',
+                                                 'rgba(255, 159, 64, 0.2)',
+                                             ],
+                                             borderColor: [
+                                                 'rgba(255, 99, 132, 1)',
+                                                 'rgba(54, 162, 235, 1)',
+                                                 'rgba(255, 206, 86, 1)',
+                                                 'rgba(75, 192, 192, 1)',
+                                                 'rgba(153, 102, 255, 1)',
+                                                 'rgba(255, 159, 64, 1)',
+                                                 'rgba(255, 99, 132, 1)',
+                                                 'rgba(54, 162, 235, 1)',
+                                                 'rgba(255, 206, 86, 1)',
+                                                 'rgba(75, 192, 192, 1)',
+                                                 'rgba(153, 102, 255, 1)',
+                                                 'rgba(255, 159, 64, 1)',
+                                                 'rgba(255, 99, 132, 1)',
+                                                 'rgba(54, 162, 235, 1)',
+                                                 'rgba(255, 206, 86, 1)',
+                                                 'rgba(75, 192, 192, 1)',
+                                                 'rgba(153, 102, 255, 1)',
+                                                 'rgba(255, 159, 64, 1)',
+                                                 'rgba(255, 99, 132, 1)',
+                                                 'rgba(54, 162, 235, 1)',
+                                                 'rgba(255, 206, 86, 1)',
+                                                 'rgba(75, 192, 192, 1)',
+                                                 'rgba(153, 102, 255, 1)',
+                                                 'rgba(255, 159, 64, 1)',
+                                             ],
+                                             borderWidth: 1
+                                         }]
+            });
+        })
+        .catch(err =>{
+            console.log(err);
+        })
+    }
+    useEffect(() => {
+        Chart();
+      }, []);
+      return(
+          <div className="App">
+              <div>
+                  <Bar
+                    data={chartData}
+                    options={{
+                        responsive:true,
+                        title: { text: "THICCNESS SCALE", display: true },
+                        scales:{
+                            yAxes:{
+                                ticks:{
+                                    beginAtZero: true
+                                }
+                            }
+                        }
+                    }}
+                  />
+              </div>
+          </div>
+      )
+}
+
+export default OrderBarChart;
