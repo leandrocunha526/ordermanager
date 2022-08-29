d980e0697cde498f30788d8fb4dfc397e9e3ea4d
Leandro Cunha <leandrocunha016@gmail.com>
2022-08-28 21:50:06 -0300

    Migrate graphics improvements

--- src/components/charts/AgriculturalInputBarChart.jsx
+++ src/components/charts/AgriculturalInputBarChart.jsx
@@ -3,109 +3,108 @@ import { Bar } from "react-chartjs-2";
 import api from "../../services/api";
 
 const AgriculturalInputBarChart = () => {
-  const [chartData, setChartData] = useState({});
+    const [chartData, setChartData] = useState({});
 
-  const Chart = () => {
-    let agriculturalinputs_price = [];
-    let agriculturalinputs_quantity = [];
+    const Chart = () => {
+        let agriculturalinputs_price = [];
+        let agriculturalinputs_name = [];
 
-    api
-      .get("/api/agriculturalinputs/list")
-      .then((res) => {
-        for (const dataObj of res.data) {
-          agriculturalinputs_price.push(parseFloat(dataObj.price));
-          agriculturalinputs_quantity.push(parseInt(dataObj.quantity));
-        }
-        setChartData({
-          labels: agriculturalinputs_quantity,
-          datasets: [
-            {
-              label: "Preço do insumo (R$)",
-              data: agriculturalinputs_price,
-              backgroundColor: [
-                "rgba(255, 99, 132, 0.2)",
-                "rgba(54, 162, 235, 0.2)",
-                "rgba(255, 206, 86, 0.2)",
-                "rgba(75, 192, 192, 0.2)",
-                "rgba(153, 102, 255, 0.2)",
-                "rgba(255, 159, 64, 0.2)",
-                "rgba(255, 99, 132, 0.2)",
-                "rgba(54, 162, 235, 0.2)",
-                "rgba(255, 206, 86, 0.2)",
-                "rgba(75, 192, 192, 0.2)",
-                "rgba(153, 102, 255, 0.2)",
-                "rgba(255, 159, 64, 0.2)",
-                "rgba(255, 99, 132, 0.2)",
-                "rgba(54, 162, 235, 0.2)",
-                "rgba(255, 206, 86, 0.2)",
-                "rgba(75, 192, 192, 0.2)",
-                "rgba(153, 102, 255, 0.2)",
-                "rgba(255, 159, 64, 0.2)",
-                "rgba(255, 99, 132, 0.2)",
-                "rgba(54, 162, 235, 0.2)",
-                "rgba(255, 206, 86, 0.2)",
-                "rgba(75, 192, 192, 0.2)",
-                "rgba(153, 102, 255, 0.2)",
-                "rgba(255, 159, 64, 0.2)",
-              ],
-              borderColor: [
-                "rgba(255, 99, 132, 1)",
-                "rgba(54, 162, 235, 1)",
-                "rgba(255, 206, 86, 1)",
-                "rgba(75, 192, 192, 1)",
-                "rgba(153, 102, 255, 1)",
-                "rgba(255, 159, 64, 1)",
-                "rgba(255, 99, 132, 1)",
-                "rgba(54, 162, 235, 1)",
-                "rgba(255, 206, 86, 1)",
-                "rgba(75, 192, 192, 1)",
-                "rgba(153, 102, 255, 1)",
-                "rgba(255, 159, 64, 1)",
-                "rgba(255, 99, 132, 1)",
-                "rgba(54, 162, 235, 1)",
-                "rgba(255, 206, 86, 1)",
-                "rgba(75, 192, 192, 1)",
-                "rgba(153, 102, 255, 1)",
-                "rgba(255, 159, 64, 1)",
-                "rgba(255, 99, 132, 1)",
-                "rgba(54, 162, 235, 1)",
-                "rgba(255, 206, 86, 1)",
-                "rgba(75, 192, 192, 1)",
-                "rgba(153, 102, 255, 1)",
-                "rgba(255, 159, 64, 1)",
-              ],
-              borderWidth: 1,
-            },
-          ],
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-  useEffect(() => {
-    Chart();
-  }, []);
-  return (
-    <div className="App">
-      <div>
-        <Bar
-          data={chartData}
-          options={{
-            responsive: true,
-            title: { text: "THICCNESS SCALE", display: true },
-            scales: {
-              yAxes: {
-                ticks: {
-                  beginAtZero: true,
-                },
-              },
-            },
-          }}
-        />
-      </div>
-    </div>
-  );
+        api.get("/api/agriculturalinputs/list")
+            .then((res) => {
+                for (const dataObj of res.data) {
+                    agriculturalinputs_price.push(parseFloat(dataObj.price));
+                    agriculturalinputs_name.push(dataObj.name);
+                }
+                setChartData({
+                    labels: agriculturalinputs_name,
+                    datasets: [
+                        {
+                            label: "Preço do insumo (R$)",
+                            data: agriculturalinputs_price,
+                            backgroundColor: [
+                                "rgba(255, 99, 132, 0.2)",
+                                "rgba(54, 162, 235, 0.2)",
+                                "rgba(255, 206, 86, 0.2)",
+                                "rgba(75, 192, 192, 0.2)",
+                                "rgba(153, 102, 255, 0.2)",
+                                "rgba(255, 159, 64, 0.2)",
+                                "rgba(255, 99, 132, 0.2)",
+                                "rgba(54, 162, 235, 0.2)",
+                                "rgba(255, 206, 86, 0.2)",
+                                "rgba(75, 192, 192, 0.2)",
+                                "rgba(153, 102, 255, 0.2)",
+                                "rgba(255, 159, 64, 0.2)",
+                                "rgba(255, 99, 132, 0.2)",
+                                "rgba(54, 162, 235, 0.2)",
+                                "rgba(255, 206, 86, 0.2)",
+                                "rgba(75, 192, 192, 0.2)",
+                                "rgba(153, 102, 255, 0.2)",
+                                "rgba(255, 159, 64, 0.2)",
+                                "rgba(255, 99, 132, 0.2)",
+                                "rgba(54, 162, 235, 0.2)",
+                                "rgba(255, 206, 86, 0.2)",
+                                "rgba(75, 192, 192, 0.2)",
+                                "rgba(153, 102, 255, 0.2)",
+                                "rgba(255, 159, 64, 0.2)",
+                            ],
+                            borderColor: [
+                                "rgba(255, 99, 132, 1)",
+                                "rgba(54, 162, 235, 1)",
+                                "rgba(255, 206, 86, 1)",
+                                "rgba(75, 192, 192, 1)",
+                                "rgba(153, 102, 255, 1)",
+                                "rgba(255, 159, 64, 1)",
+                                "rgba(255, 99, 132, 1)",
+                                "rgba(54, 162, 235, 1)",
+                                "rgba(255, 206, 86, 1)",
+                                "rgba(75, 192, 192, 1)",
+                                "rgba(153, 102, 255, 1)",
+                                "rgba(255, 159, 64, 1)",
+                                "rgba(255, 99, 132, 1)",
+                                "rgba(54, 162, 235, 1)",
+                                "rgba(255, 206, 86, 1)",
+                                "rgba(75, 192, 192, 1)",
+                                "rgba(153, 102, 255, 1)",
+                                "rgba(255, 159, 64, 1)",
+                                "rgba(255, 99, 132, 1)",
+                                "rgba(54, 162, 235, 1)",
+                                "rgba(255, 206, 86, 1)",
+                                "rgba(75, 192, 192, 1)",
+                                "rgba(153, 102, 255, 1)",
+                                "rgba(255, 159, 64, 1)",
+                            ],
+                            borderWidth: 1,
+                        },
+                    ],
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+    useEffect(() => {
+        Chart();
+    }, []);
+    return (
+        <div className="App">
+            <div>
+                <Bar
+                    data={chartData}
+                    options={{
+                        responsive: true,
+                        title: { text: "THICCNESS SCALE", display: true },
+                        scales: {
+                            yAxes: {
+                                ticks: {
+                                    beginAtZero: true,
+                                },
+                            },
+                        },
+                    }}
+                />
+            </div>
+        </div>
+    );
 };
 
 export default AgriculturalInputBarChart;
--- src/components/charts/EmployeeBarChart.jsx
+++ src/components/charts/EmployeeBarChart.jsx
@@ -1,110 +1,110 @@
-import React, { useState, useEffect } from 'react'
-import {Bar} from 'react-chartjs-2'
+import React, { useState, useEffect } from "react";
+import { Bar } from "react-chartjs-2";
 import api from "../../services/api";
 
 const EmployeeBarChart = () => {
-    const [chartData, setChartData]  = useState({});
-
+    const [chartData, setChartData] = useState({});
 
     const Chart = () => {
         let employee_salary = [];
-        let employee_birthday = [];
+        let employee_name = [];
 
         api.get("/api/employees/list")
-        .then(res => {
-            for(const dataObj of res.data){
-                employee_salary.push(parseFloat(dataObj.salary));
-                employee_birthday.push(parseInt(dataObj.birthday));
-
-            }
-            setChartData({
-                labels: employee_birthday,
-                datasets: [{
-                                             label: 'Salário do funcionário (R$)',
-                                             data: employee_salary,
-                                             backgroundColor: [
-                                                 'rgba(255, 99, 132, 0.2)',
-                                                 'rgba(54, 162, 235, 0.2)',
-                                                 'rgba(255, 206, 86, 0.2)',
-                                                 'rgba(75, 192, 192, 0.2)',
-                                                 'rgba(153, 102, 255, 0.2)',
-                                                 'rgba(255, 159, 64, 0.2)',
-                                                 'rgba(255, 99, 132, 0.2)',
-                                                 'rgba(54, 162, 235, 0.2)',
-                                                 'rgba(255, 206, 86, 0.2)',
-                                                 'rgba(75, 192, 192, 0.2)',
-                                                 'rgba(153, 102, 255, 0.2)',
-                                                 'rgba(255, 159, 64, 0.2)',
-                                                 'rgba(255, 99, 132, 0.2)',
-                                                 'rgba(54, 162, 235, 0.2)',
-                                                 'rgba(255, 206, 86, 0.2)',
-                                                 'rgba(75, 192, 192, 0.2)',
-                                                 'rgba(153, 102, 255, 0.2)',
-                                                 'rgba(255, 159, 64, 0.2)',
-                                                 'rgba(255, 99, 132, 0.2)',
-                                                 'rgba(54, 162, 235, 0.2)',
-                                                 'rgba(255, 206, 86, 0.2)',
-                                                 'rgba(75, 192, 192, 0.2)',
-                                                 'rgba(153, 102, 255, 0.2)',
-                                                 'rgba(255, 159, 64, 0.2)',
-                                             ],
-                                             borderColor: [
-                                                 'rgba(255, 99, 132, 1)',
-                                                 'rgba(54, 162, 235, 1)',
-                                                 'rgba(255, 206, 86, 1)',
-                                                 'rgba(75, 192, 192, 1)',
-                                                 'rgba(153, 102, 255, 1)',
-                                                 'rgba(255, 159, 64, 1)',
-                                                 'rgba(255, 99, 132, 1)',
-                                                 'rgba(54, 162, 235, 1)',
-                                                 'rgba(255, 206, 86, 1)',
-                                                 'rgba(75, 192, 192, 1)',
-                                                 'rgba(153, 102, 255, 1)',
-                                                 'rgba(255, 159, 64, 1)',
-                                                 'rgba(255, 99, 132, 1)',
-                                                 'rgba(54, 162, 235, 1)',
-                                                 'rgba(255, 206, 86, 1)',
-                                                 'rgba(75, 192, 192, 1)',
-                                                 'rgba(153, 102, 255, 1)',
-                                                 'rgba(255, 159, 64, 1)',
-                                                 'rgba(255, 99, 132, 1)',
-                                                 'rgba(54, 162, 235, 1)',
-                                                 'rgba(255, 206, 86, 1)',
-                                                 'rgba(75, 192, 192, 1)',
-                                                 'rgba(153, 102, 255, 1)',
-                                                 'rgba(255, 159, 64, 1)',
-                                             ],
-                                             borderWidth: 1
-                                         }]
+            .then((res) => {
+                for (const dataObj of res.data) {
+                    employee_salary.push(parseFloat(dataObj.salary));
+                    employee_name.push(dataObj.name);
+                }
+                setChartData({
+                    labels: employee_name,
+                    datasets: [
+                        {
+                            label: "Salário do funcionário (R$)",
+                            data: employee_salary,
+                            backgroundColor: [
+                                "rgba(255, 99, 132, 0.2)",
+                                "rgba(54, 162, 235, 0.2)",
+                                "rgba(255, 206, 86, 0.2)",
+                                "rgba(75, 192, 192, 0.2)",
+                                "rgba(153, 102, 255, 0.2)",
+                                "rgba(255, 159, 64, 0.2)",
+                                "rgba(255, 99, 132, 0.2)",
+                                "rgba(54, 162, 235, 0.2)",
+                                "rgba(255, 206, 86, 0.2)",
+                                "rgba(75, 192, 192, 0.2)",
+                                "rgba(153, 102, 255, 0.2)",
+                                "rgba(255, 159, 64, 0.2)",
+                                "rgba(255, 99, 132, 0.2)",
+                                "rgba(54, 162, 235, 0.2)",
+                                "rgba(255, 206, 86, 0.2)",
+                                "rgba(75, 192, 192, 0.2)",
+                                "rgba(153, 102, 255, 0.2)",
+                                "rgba(255, 159, 64, 0.2)",
+                                "rgba(255, 99, 132, 0.2)",
+                                "rgba(54, 162, 235, 0.2)",
+                                "rgba(255, 206, 86, 0.2)",
+                                "rgba(75, 192, 192, 0.2)",
+                                "rgba(153, 102, 255, 0.2)",
+                                "rgba(255, 159, 64, 0.2)",
+                            ],
+                            borderColor: [
+                                "rgba(255, 99, 132, 1)",
+                                "rgba(54, 162, 235, 1)",
+                                "rgba(255, 206, 86, 1)",
+                                "rgba(75, 192, 192, 1)",
+                                "rgba(153, 102, 255, 1)",
+                                "rgba(255, 159, 64, 1)",
+                                "rgba(255, 99, 132, 1)",
+                                "rgba(54, 162, 235, 1)",
+                                "rgba(255, 206, 86, 1)",
+                                "rgba(75, 192, 192, 1)",
+                                "rgba(153, 102, 255, 1)",
+                                "rgba(255, 159, 64, 1)",
+                                "rgba(255, 99, 132, 1)",
+                                "rgba(54, 162, 235, 1)",
+                                "rgba(255, 206, 86, 1)",
+                                "rgba(75, 192, 192, 1)",
+                                "rgba(153, 102, 255, 1)",
+                                "rgba(255, 159, 64, 1)",
+                                "rgba(255, 99, 132, 1)",
+                                "rgba(54, 162, 235, 1)",
+                                "rgba(255, 206, 86, 1)",
+                                "rgba(75, 192, 192, 1)",
+                                "rgba(153, 102, 255, 1)",
+                                "rgba(255, 159, 64, 1)",
+                            ],
+                            borderWidth: 1,
+                        },
+                    ],
+                });
+            })
+            .catch((err) => {
+                console.log(err);
             });
-        })
-        .catch(err =>{
-            console.log(err);
-        })
-    }
+    };
     useEffect(() => {
         Chart();
-      }, []);
-      return(
-          <div className="App">
-              <div>
-                  <Bar
+    }, []);
+    return (
+        <div className="App">
+            <div>
+                <Bar
                     data={chartData}
                     options={{
-                        responsive:true,
+                        responsive: true,
                         title: { text: "THICCNESS SCALE", display: true },
-                        scales:{
-                            yAxes:{
-                                ticks:{
-                                    beginAtZero: true
-                                }
-                            }
-                        }
+                        scales: {
+                            yAxes: {
+                                ticks: {
+                                    beginAtZero: true,
+                                },
+                            },
+                        },
                     }}
-                  />
-              </div>
-          </div>
-      )
-}
+                />
+            </div>
+        </div>
+    );
+};
 
 export default EmployeeBarChart;
