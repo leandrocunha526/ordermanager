37203374e61c25d17ec2c699f372f5275e470097
Leandro Cunha <leandrocunha016@gmail.com>
2022-08-28 21:50:32 -0300

    Migrate sidebar and navbar improvements

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,30 +1,34 @@
 import React from "react";
 import "./Navbar.css";
 import avatar from "../../assets/avatar.svg";
-import {Link} from "react-router-dom";
-import Logout from "../../components/logout"
+import { NavLink } from "react-router-dom";
+import Logout from "../../components/logout";
 
 const Navbar = ({ sidebarOpen, openSidebar }) => {
-  return (
-    <nav className="navbar">
-      <div className="nav_icon" onClick={() => openSidebar()}>
-        <i className="fa fa-bars"></i>
-      </div>
-      <div className="navbar__left">
-        <Link to="/dashboard" className="active_link">
-          Admin
-        </Link>
-        <Link to="/orders/list">Ordens de serviço</Link>
-        <Link to="/users/list">Usuários</Link>
-      </div>
-      <div className="navbar__right">
-        <a href="/dashboard">
-          <img width="30" src={avatar} alt="avatar" />
-        </a>
-        <Logout/>
-      </div>
-    </nav>
-  );
+    return (
+        <nav className="navbar">
+            <div className="nav_icon" onClick={() => openSidebar()}>
+                <i className="fa fa-bars"></i>
+            </div>
+            <div className="navbar__left">
+                <NavLink to="/dashboard" activeClassName="active_link">
+                    Home
+                </NavLink>
+                <NavLink to="/orders/list" activeClassName="active_link">
+                    Ordens
+                </NavLink>
+                <NavLink to="/users/list" activeClassName="active_link">
+                    Usuários
+                </NavLink>
+            </div>
+            <div className="navbar__right">
+                <a href="/dashboard">
+                    <img width="30" src={avatar} alt="avatar" />
+                </a>
+                <Logout />
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -2,82 +2,79 @@ import React from "react";
 import "./Sidebar.css";
 import { Link, withRouter } from "react-router-dom";
 
-const Sidebar = ({sidebarOpen, closeSidebar}) => {
-  return (
-    <div className={sidebarOpen ? "sidebar__responsive" : ""} id="sidebar">
-      <div className="sidebar__title">
-        <div className="sidebar__img">
-          <h1>Order manager</h1>
+const Sidebar = ({ sidebarOpen, closeSidebar }) => {
+    return (
+        <div className={sidebarOpen ? "sidebar__responsive" : ""} id="sidebar">
+            <div className="sidebar__title">
+                <div className="sidebar__img">
+                    <h1>Order manager</h1>
+                </div>
+                <i
+                    className="fa fa-times"
+                    id="sidebarIcon"
+                    onClick={() => closeSidebar()}
+                ></i>
+            </div>
+            <div className="sidebar__menu">
+                <h2>Agendamentos</h2>
+                <div className="sidebar__link">
+                    <i className="fa fa-calendar"></i>
+                    <Link to="/orders/register">Agendar ordem de serviço</Link>
+                </div>
+                <h2>Cadastros</h2>
+                <div className="sidebar__link">
+                    <i className="fa fa-industry"></i>
+                    <Link to="/manufacturer/register">Cadastro de marca</Link>
+                </div>
+                <div className="sidebar__link">
+                    <i className="fas fa-tractor"></i>
+                    <Link to="/models/register">Cadastro de modelo</Link>
+                </div>
+                <div className="sidebar__link">
+                    <i className="fas fa-tractor"></i>
+                    <Link to="/machine/register">Cadastro de máquina</Link>
+                </div>
+                <div className="sidebar__link">
+                    <i className="fa fa-industry"></i>
+                    <Link to="/provider/register">Cadastro de fornecedor</Link>
+                </div>
+                <div className="sidebar__link">
+                    <i className="fas fa-seedling"></i>
+                    <Link to="/agriculturalinput/register">
+                        Cadastro de insumo
+                    </Link>
+                </div>
+                <div className="sidebar__link">
+                    <i className="fas fa-users-cog"></i>
+                    <Link to="/employee/register">Cadastro de funcionário</Link>
+                </div>
+                <h2>Listagens</h2>
+                <div className="sidebar__link">
+                    <i className="fa fa-industry"></i>
+                    <Link to="/manufacturer/list">Marcas</Link>
+                </div>
+                <div className="sidebar__link">
+                    <i className="fas fa-tractor"></i>
+                    <Link to="/models">Modelos</Link>
+                </div>
+                <div className="sidebar__link">
+                    <i className="fas fa-tractor"></i>
+                    <Link to="/machine/list">Máquinas</Link>
+                </div>
+                <div className="sidebar__link">
+                    <i className="fa fa-industry"></i>
+                    <Link to="/provider/list">Fornecedores</Link>
+                </div>
+                <div className="sidebar__link">
+                    <i className="fas fa-seedling"></i>
+                    <Link to="/agriculturalinput/list">Insumos</Link>
+                </div>
+                <div className="sidebar__link">
+                    <i className="fas fa-users-cog"></i>
+                    <Link to="/employee/list">Funcionários</Link>
+                </div>
+            </div>
         </div>
-        <i
-          className="fa fa-times"
-          id="sidebarIcon"
-          onClick={() => closeSidebar()}
-        ></i>
-      </div>
-      <div className="sidebar__menu">
-      <h2>Dashboard</h2>
-        <div className="sidebar__link active_menu_link">
-          <i className="fa fa-home"></i>
-          <Link to="/dashboard">Home</Link>
-        </div>
-        <h2>Agendamentos</h2>
-        <div className="sidebar__link">
-          <i className="fa fa-calendar"></i>
-          <Link to="/orders/register">Agendar ordem de serviço</Link>
-        </div>
-        <h2>Cadastros</h2>
-        <div className="sidebar__link">
-          <i className="fa fa-industry"></i>
-          <Link to="/manufacturer/register">Cadastro de marca</Link>
-        </div>
-        <div className="sidebar__link">
-          <i className="fas fa-tractor"></i>
-          <Link to="/models/register">Cadastro de modelo</Link>
-        </div>
-        <div className="sidebar__link">
-          <i className="fas fa-tractor"></i>
-          <Link to="/machine/register">Cadastro de máquina</Link>
-        </div>
-        <div className="sidebar__link">
-          <i className="fa fa-industry"></i>
-          <Link to="/provider/register">Cadastro de fornecedor</Link>
-        </div>
-        <div className="sidebar__link">
-          <i className="fas fa-seedling"></i>
-          <Link to="/agriculturalinput/register">Cadastro de insumo</Link>
-        </div>
-        <div className="sidebar__link">
-          <i className="fas fa-users-cog"></i>
-          <Link to="/employee/register">Cadastro de funcionário</Link>
-        </div>
-        <h2>Listagens</h2>
-        <div className="sidebar__link">
-          <i className="fa fa-industry"></i>
-          <Link to="/manufacturer/list">Marcas</Link>
-        </div>
-        <div className="sidebar__link">
-          <i className="fas fa-tractor"></i>
-          <Link to="/models">Modelos</Link>
-        </div>
-        <div className="sidebar__link">
-          <i className="fas fa-tractor"></i>
-          <Link to="/machine/list">Máquinas</Link>
-        </div>
-        <div className="sidebar__link">
-          <i className="fa fa-industry"></i>
-          <Link to="/provider/list">Fornecedores</Link>
-        </div>
-        <div className="sidebar__link">
-          <i className="fas fa-seedling"></i>
-          <Link to="/agriculturalinput/list">Insumos</Link>
-        </div>
-        <div className="sidebar__link">
-          <i className="fas fa-users-cog"></i>
-          <Link to="/employee/list">Funcionários</Link>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 export default withRouter(Sidebar);
