1823f3688819eb11deffd8d34439d38b49948ac7
Leandro Cunha <leandrocunha016@gmail.com>
2023-02-07 16:57:48 -0300

    Add MUI alert in agricultural input

--- src/pages/AgriculturalInput/AgriculturalInput.js
+++ src/pages/AgriculturalInput/AgriculturalInput.js
@@ -3,130 +3,168 @@ import { withRouter } from "react-router-dom";
 import api from "../../services/api";
 import { Form, Container } from "./styles/AgriculturalInputForm";
 import Title from "../../components/title";
+import { Alert, AlertTitle } from "@mui/material";
 
 class AgriculturalInput extends Component {
-  state = {
-    name: "",
-    sprayrate: "",
-    price: "",
-    quantity: "",
-    providerId: "",
-    providers: [],
-    acquisitionDate: "",
-    message: "",
-    error: "",
-  };
+    state = {
+        name: "",
+        sprayrate: "",
+        price: "",
+        quantity: "",
+        providerId: "",
+        providers: [],
+        acquisitionDate: "",
+        message: "",
+        error: "",
+    };
 
-  componentDidMount() {
-    api
-      .get("api/providers/list")
-      .then((res) => {
-        const providers = res.data;
-        this.setState({ providers });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-
-  handleRegister = async (e) => {
-    e.preventDefault();
-    const { name, sprayrate, price, quantity, providerId, acquisitionDate } = this.state;
-    if (!name || !sprayrate || !price || !quantity || !providerId || !acquisitionDate) {
-      this.setState({
-        error: "Preencha todos os campos para cadastrar um insumo",
-      });
-    } else {
-      try {
-        await api.post("/api/agriculturalinputs/register", {
-          name,
-          sprayrate,
-          price,
-          quantity,
-          providerId,
-          acquisitionDate
-        });
-        this.setState({
-          message: "Salvo com sucesso",
-        });
-      } catch (err) {
-        this.setState({
-          error: "Ocorreu o seguinte problema com o cadastro: " + err,
-        });
-      }
+    componentDidMount() {
+        api.get("api/providers/list")
+            .then((res) => {
+                const providers = res.data;
+                this.setState({ providers });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     }
-  };
 
-  render() {
-    return (
-      <main>
-        <Container>
-          <Form onSubmit={this.handleRegister}>
-            <Title title="Cadastro de insumo agrícola" />
-            {this.state.message && <p>{this.state.message}</p>}
-            {this.state.error && <p>{this.state.error}</p>}
+    handleRegister = async (e) => {
+        e.preventDefault();
+        const {
+            name,
+            sprayrate,
+            price,
+            quantity,
+            providerId,
+            acquisitionDate,
+        } = this.state;
+        if (
+            !name ||
+            !sprayrate ||
+            !price ||
+            !quantity ||
+            !providerId ||
+            !acquisitionDate
+        ) {
+            this.setState({
+                error: "Preencha todos os campos para cadastrar um insumo.",
+            });
+        } else {
+            try {
+                await api.post("/api/agriculturalinputs/register", {
+                    name,
+                    sprayrate,
+                    price,
+                    quantity,
+                    providerId,
+                    acquisitionDate,
+                });
+                this.setState({
+                    message: "Foi cadastrado um novo insumo.",
+                });
+            } catch (err) {
+                this.setState({
+                    error: "Ocorreu o seguinte problema com o cadastro: " + err,
+                });
+            }
+        }
+    };
 
-            <label>Nome</label>
-            <input
-              type="text"
-              placeholder="Nome"
-              value={this.state.name}
-              onChange={(e) => this.setState({ name: e.target.value })}
-            />
+    render() {
+        return (
+            <main>
+                <Container>
+                    <Form onSubmit={this.handleRegister}>
+                        <Title title="Cadastro de insumo agrícola" />
+                        {this.state.message && (
+                            <Alert severity="success">
+                                <AlertTitle>Sucesso!</AlertTitle>
+                                {this.state.message}
+                            </Alert>
+                        )}
+                        {this.state.error && (
+                            <Alert severity="error">
+                                <AlertTitle>Erro!</AlertTitle>
+                                {this.state.error}
+                            </Alert>
+                        )}
 
-            <label>Taxa de pulverização</label>
-            <input
-              type="text"
-              placeholder="Taxa de pulverização"
-              value={this.state.sprayrate}
-              onChange={(e) => this.setState({ sprayrate: e.target.value })}
-            />
+                        <label>Nome</label>
+                        <input
+                            type="text"
+                            placeholder="Nome"
+                            value={this.state.name}
+                            onChange={(e) =>
+                                this.setState({ name: e.target.value })
+                            }
+                        />
 
-            <label>Preço</label>
-            <input
-              type="number"
-              placeholder="Preço"
-              value={this.state.price}
-              onChange={(e) => this.setState({ price: e.target.value })}
-            />
+                        <label>Taxa de pulverização</label>
+                        <input
+                            type="text"
+                            placeholder="Taxa de pulverização"
+                            value={this.state.sprayrate}
+                            onChange={(e) =>
+                                this.setState({ sprayrate: e.target.value })
+                            }
+                        />
 
-            <label>Quantidade</label>
-            <input
-              type="number"
-              placeholder="Quantidade"
-              value={this.state.quantity}
-              onChange={(e) => this.setState({ quantity: e.target.value })}
-            />
+                        <label>Preço</label>
+                        <input
+                            type="number"
+                            placeholder="Preço"
+                            value={this.state.price}
+                            onChange={(e) =>
+                                this.setState({ price: e.target.value })
+                            }
+                        />
 
-            <label>Data de aquisição</label>
-            <input
-              type="date"
-              placeholder="Data de aquisição"
-              value={this.state.acquisitionDate}
-              onChange={(e) => this.setState({ acquisitionDate: e.target.value })}
-            />
+                        <label>Quantidade</label>
+                        <input
+                            type="number"
+                            placeholder="Quantidade"
+                            value={this.state.quantity}
+                            onChange={(e) =>
+                                this.setState({ quantity: e.target.value })
+                            }
+                        />
 
-            <label>Fornecedor</label>
-            <select
-              name="providerId"
-              id="providerId"
-              value={this.state.providerId}
-              onChange={(e) => this.setState({ providerId: e.target.value })}
-            >
-              <option value="0">Selecione um fornecedor</option>
+                        <label>Data de aquisição</label>
+                        <input
+                            type="date"
+                            placeholder="Data de aquisição"
+                            value={this.state.acquisitionDate}
+                            onChange={(e) =>
+                                this.setState({
+                                    acquisitionDate: e.target.value,
+                                })
+                            }
+                        />
 
-              {this.state.providers.map((provider) => (
-                <option key={provider.id} value={provider.id}>
-                  {provider.corporateName}
-                </option>
-              ))}
-            </select>
-            <button type="submit">Enviar</button>
-          </Form>
-        </Container>
-      </main>
-    );
-  }
+                        <label>Fornecedor</label>
+                        <select
+                            name="providerId"
+                            id="providerId"
+                            value={this.state.providerId}
+                            onChange={(e) =>
+                                this.setState({ providerId: e.target.value })
+                            }
+                        >
+                            <option value="0">Selecione um fornecedor</option>
+
+                            {this.state.providers.map((provider) => (
+                                <option key={provider.id} value={provider.id}>
+                                    {provider.corporateName}
+                                </option>
+                            ))}
+                        </select>
+                        <button type="submit">Enviar</button>
+                    </Form>
+                </Container>
+            </main>
+        );
+    }
 }
 
 export default withRouter(AgriculturalInput);
--- src/pages/AgriculturalInput/AgriculturalInputTable.js
+++ src/pages/AgriculturalInput/AgriculturalInputTable.js
@@ -2,95 +2,129 @@ import React, { Component } from "react";
 import api from "../../services/api";
 import { withRouter, Link } from "react-router-dom";
 import "./styles/AgriculturalInputTable.css";
-import Moment from 'react-moment';
+import Moment from "react-moment";
+import { Alert, AlertTitle } from "@mui/material";
 
 class AgriculturalInputTable extends Component {
-  state = {
-    agriculturalinputs: [],
-    message: "",
-    error: "",
-  };
+    state = {
+        agriculturalinputs: [],
+        message: "",
+        error: "",
+    };
 
-  componentDidMount() {
-    api
-      .get("/api/agriculturalinputs/list")
-      .then((res) => {
-        const agriculturalinputs = res.data;
-        this.setState({ agriculturalinputs });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-  async delete(id) {
-    try {
-      await api.delete(`/api/agriculturalinputs/${id}`);
-      this.setState({
-        message: "Insumo excluído com sucesso",
-      });
-    } catch (err) {
-      this.setState({
-        error: "Ocorreu o seguinte problema ao excluir o insumo: " + err,
-      });
+    componentDidMount() {
+        api.get("/api/agriculturalinputs/list")
+            .then((res) => {
+                const agriculturalinputs = res.data;
+                this.setState({ agriculturalinputs });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     }
-  }
-  render() {
-    return (
-      <main>
-        <div className="usertable__container">
-          <div>
-            <h1>Insumos cadastrados</h1>
-          </div>
-          {this.state.message && <h3>{this.state.message}</h3>}
-          {this.state.error && <h3>{this.state.error}</h3>}
-          <table>
-            <thead>
-              <tr>
-                <th>Id</th>
-                <th>Nome</th>
-                <th>Taxa de pulverização</th>
-                <th>Preço</th>
-                <th>Quantidade</th>
-                <th>Fornecedor</th>
-                <th>Data de aquisição</th>
-                <th>Deletar</th>
-                <th>Editar</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.state.agriculturalinputs.map((agriculturalinput, index) => (
-                <tr key={index}>
-                  <td>{agriculturalinput.id}</td>
-                  <td>{agriculturalinput.name}</td>
-                  <td>{agriculturalinput.sprayrate}</td>
-                  <td>{agriculturalinput.price}</td>
-                  <td>{agriculturalinput.quantity}</td>
-                  <td>{agriculturalinput.providers.corporateName}</td>
-                  <td><Moment format="DD/MM/YYYY">{agriculturalinput.acquisitionDate}</Moment></td>
-                  <td>
-                    <button
-                      type="button"
-                      className="button__warning"
-                      onClick={() => this.delete(agriculturalinput.id)}
-                    >
-                      Deletar
-                    </button>
-                  </td>
-                  <td>
-                    <Link
-                      to={"/agriculturalinput/edit/" + agriculturalinput.id}
-                      className="button__primary"
-                    >
-                      Editar
-                    </Link>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </main>
-    );
-  }
+    async delete(id) {
+        try {
+            await api.delete(`/api/agriculturalinputs/${id}`);
+            this.setState({
+                message: "O insumo " + id + " foi excluído.",
+            });
+        } catch (err) {
+            this.setState({
+                error:
+                    "Ocorreu o seguinte problema ao excluir o insumo " +
+                    id +
+                    ": " +
+                    err,
+            });
+        }
+    }
+    render() {
+        return (
+            <main>
+                <div className="usertable__container">
+                    <div>
+                        <h1>Insumos cadastrados</h1>
+                    </div>
+                    {this.state.message && (
+                        <Alert severity="success">
+                            <AlertTitle>Sucesso!</AlertTitle>
+                            {this.state.message}
+                        </Alert>
+                    )}
+                    {this.state.error && (
+                        <Alert severity="error">
+                            <AlertTitle>Erro!</AlertTitle>
+                            {this.state.error}
+                        </Alert>
+                    )}
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Id</th>
+                                <th>Nome</th>
+                                <th>Taxa de pulverização</th>
+                                <th>Preço</th>
+                                <th>Quantidade</th>
+                                <th>Fornecedor</th>
+                                <th>Data de aquisição</th>
+                                <th>Deletar</th>
+                                <th>Editar</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.state.agriculturalinputs.map(
+                                (agriculturalinput, index) => (
+                                    <tr key={index}>
+                                        <td>{agriculturalinput.id}</td>
+                                        <td>{agriculturalinput.name}</td>
+                                        <td>{agriculturalinput.sprayrate}</td>
+                                        <td>{agriculturalinput.price}</td>
+                                        <td>{agriculturalinput.quantity}</td>
+                                        <td>
+                                            {
+                                                agriculturalinput.providers
+                                                    .corporateName
+                                            }
+                                        </td>
+                                        <td>
+                                            <Moment format="DD/MM/YYYY">
+                                                {
+                                                    agriculturalinput.acquisitionDate
+                                                }
+                                            </Moment>
+                                        </td>
+                                        <td>
+                                            <button
+                                                type="button"
+                                                className="button__warning"
+                                                onClick={() =>
+                                                    this.delete(
+                                                        agriculturalinput.id
+                                                    )
+                                                }
+                                            >
+                                                Deletar
+                                            </button>
+                                        </td>
+                                        <td>
+                                            <Link
+                                                to={
+                                                    "/agriculturalinput/edit/" +
+                                                    agriculturalinput.id
+                                                }
+                                                className="button__primary"
+                                            >
+                                                Editar
+                                            </Link>
+                                        </td>
+                                    </tr>
+                                )
+                            )}
+                        </tbody>
+                    </table>
+                </div>
+            </main>
+        );
+    }
 }
 export default withRouter(AgriculturalInputTable);
