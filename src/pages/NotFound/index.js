743019f39e9243d5f41f8ea82005a49566779d6a
Leandro Cunha <leandrocunha016@gmail.com>
2023-02-07 13:50:34 -0300

    Minor changes in not found page

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,30 +1,29 @@
 import { Component } from "react";
-import { withRouter } from "react-router-dom";
+import { withRouter, Link } from "react-router-dom";
 import { Container } from "./style.js";
 import Title from "../../components/title";
 import notFound from "../../assets/notfound.svg";
-import {Link} from "react-router-dom";
 
 class NotFound extends Component {
-  render() {
-    return (
-      <Container>
-        <Title title="Error 404" />
-        <p>A página que você está procurando não está aqui</p>
-        <img
-          alt="Error 404"
-          src={notFound}
-          style={{
-            marginTop: 50,
-            display: "inline-block",
-            maxWidth: "100%",
-            width: 560,
-          }}
-        />
-        <Link to="/dashboard">Retornar ao dashboard</Link>
-      </Container>
-    );
-  }
+    render() {
+        return (
+            <Container>
+                <Title title="Error 404" />
+                <p>A página que você está procurando não está aqui</p>
+                <img
+                    alt="Error 404"
+                    src={notFound}
+                    style={{
+                        marginTop: 50,
+                        display: "inline-block",
+                        maxWidth: "100%",
+                        width: 560,
+                    }}
+                />
+                <Link to="/dashboard">Retornar ao dashboard</Link>
+            </Container>
+        );
+    }
 }
 
 export default withRouter(NotFound);
