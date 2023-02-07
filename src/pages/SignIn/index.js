d529f274b699bbc5889aa19ab0e18136821ccea2
Leandro Cunha <leandrocunha016@gmail.com>
2023-02-07 18:28:12 -0300

    Add MUI alerts to signin and signup

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -4,68 +4,78 @@ import api from "../../services/api";
 import { login } from "../../services/auth";
 import { Form, Container } from "./styles";
 import Title from "../../components/title";
+import { Alert, AlertTitle } from "@mui/material";
 
 class SignIn extends Component {
-  state = {
-    username: "",
-    password: "",
-    error: "",
-  };
+    state = {
+        username: "",
+        password: "",
+        error: "",
+    };
 
-  handleLogin = async (e) => {
-    e.preventDefault();
-    const { username, password } = this.state;
-    if (!username || !password) {
-      this.setState({
-        error: "Preencha seu nome de usuário e senha para continuar",
-      });
-    } else {
-      try {
-        const response = await api.post("/api/users/authenticate", {
-          username: username,
-          password: password,
-          withCredentials: true,
-          headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Headers": "Authorization",
-            "Access-Control-Allow-Methods": "POST",
-            "Content-Type": "application/json;charset=UTF-8",
-          },
-        });
-        login(response.data.token);
-        this.props.history.push("/dashboard");
-      } catch (err) {
-        this.setState({
-          error: "Ocorreu um problema com o login, verifique suas credenciais. " + err,
-        });
-      }
+    handleLogin = async (e) => {
+        e.preventDefault();
+        const { username, password } = this.state;
+        if (!username || !password) {
+            this.setState({
+                error: "Preencha seu nome de usuário e senha para continuar",
+            });
+        } else {
+            try {
+                const response = await api.post("/api/users/authenticate", {
+                    username: username,
+                    password: password,
+                    withCredentials: true,
+                    headers: {
+                        "Access-Control-Allow-Origin": "*",
+                        "Access-Control-Allow-Headers": "Authorization",
+                        "Access-Control-Allow-Methods": "POST",
+                        "Content-Type": "application/json;charset=UTF-8",
+                    },
+                });
+                login(response.data.token);
+                this.props.history.push("/dashboard");
+            } catch (err) {
+                this.setState({
+                    error: "Ocorreu um problema com o login, verifique suas credenciais.",
+                });
+            }
+        }
+    };
+    render() {
+        return (
+            <Container>
+                <Form onSubmit={this.handleLogin}>
+                    <Title title="Entrar" />
+                    {this.state.error && (
+                        <Alert severity="error">
+                            <AlertTitle>Erro!</AlertTitle>
+                            {this.state.error}
+                        </Alert>
+                    )}
+                    <input
+                        type="text"
+                        placeholder="Nome de usuário"
+                        value={this.state.username}
+                        onChange={(e) =>
+                            this.setState({ username: e.target.value })
+                        }
+                    />
+                    <input
+                        type="password"
+                        placeholder="Senha"
+                        value={this.state.password}
+                        onChange={(e) =>
+                            this.setState({ password: e.target.value })
+                        }
+                    />
+                    <button type="submit">Entrar</button>
+                    <hr />
+                    <Link to="/signup">Registrar</Link>
+                </Form>
+            </Container>
+        );
     }
-  };
-  render() {
-    return (
-      <Container>
-        <Form onSubmit={this.handleLogin}>
-          <Title title="Entrar" />
-          {this.state.error && <p>{this.state.error}</p>}
-          <input
-            type="text"
-            placeholder="Nome de usuário"
-            value={this.state.username}
-            onChange={(e) => this.setState({ username: e.target.value })}
-          />
-          <input
-            type="password"
-            placeholder="Senha"
-            value={this.state.password}
-            onChange={(e) => this.setState({ password: e.target.value })}
-          />
-          <button type="submit">Entrar</button>
-          <hr />
-          <Link to="/signup">Registrar</Link>
-        </Form>
-      </Container>
-    );
-  }
 }
 
 export default withRouter(SignIn);
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -3,76 +3,99 @@ import { Link, withRouter } from "react-router-dom";
 import api from "../../services/api";
 import { Form, Container } from "./styles";
 import Title from "../../components/title";
+import { Alert, AlertTitle } from "@mui/material";
 
 class SignUp extends Component {
-  state = {
-    firstName: "",
-    lastName: "",
-    username: "",
-    password: "",
-    error: "",
-  };
+    state = {
+        firstName: "",
+        lastName: "",
+        username: "",
+        password: "",
+        error: "",
+        message: "",
+    };
 
-  handleRegister = async (e) => {
-    e.preventDefault();
-    const {firstName, lastName, username, password } = this.state;
-    if (!firstName || !lastName || !username || !password) {
-      this.setState({
-        error: "Insira todos os dados para se cadastrar",
-      });
-    } else {
-      try {
-        await api.post("/api/users/register", {
-          firstName,
-          lastName,
-          username,
-          password,
-        });
-        this.props.history.push("/");
-      } catch (err) {
-        this.setState({
-          error: "Ocorreu um problema com o cadastro ou usuário já cadastrado. " + err,
-        });
-      }
+    handleRegister = async (e) => {
+        e.preventDefault();
+        const { firstName, lastName, username, password } = this.state;
+        if (!firstName || !lastName || !username || !password) {
+            this.setState({
+                error: "Insira todos os dados para se cadastrar.",
+            });
+        } else {
+            try {
+                await api.post("/api/users/register", {
+                    firstName,
+                    lastName,
+                    username,
+                    password,
+                });
+                this.setState({
+                    message: "Usuario foi registrado.",
+                });
+            } catch (err) {
+                this.setState({
+                    error: "Ocorreu um problema com o cadastro ou usuário já cadastrado.",
+                });
+            }
+        }
+    };
+    render() {
+        return (
+            <Container>
+                <Form onSubmit={this.handleRegister}>
+                    <Title title="Registro" />
+                    {this.state.message && (
+                        <Alert severity="success">
+                            <AlertTitle>Sucesso!</AlertTitle>
+                            {this.state.message}
+                        </Alert>
+                    )}
+                    {this.state.error && (
+                        <Alert severity="error">
+                            <AlertTitle>Erro!</AlertTitle>
+                            {this.state.error}
+                        </Alert>
+                    )}
+                    <input
+                        type="text"
+                        placeholder="Primeiro Nome"
+                        value={this.state.firstName}
+                        onChange={(e) =>
+                            this.setState({ firstName: e.target.value })
+                        }
+                    />
+                    <input
+                        type="text"
+                        placeholder="Último Nome"
+                        value={this.state.lastName}
+                        onChange={(e) =>
+                            this.setState({ lastName: e.target.value })
+                        }
+                    />
+                    <input
+                        type="text"
+                        placeholder="Nome de usuário"
+                        value={this.state.username}
+                        onChange={(e) =>
+                            this.setState({ username: e.target.value })
+                        }
+                    />
+                    <input
+                        type="password"
+                        placeholder="Senha"
+                        value={this.state.password}
+                        onChange={(e) =>
+                            this.setState({ password: e.target.value })
+                        }
+                    />
+                    <button type="submit">Enviar</button>
+                    <hr />
+                    <Link to="/">Retornar para a tela de login</Link>
+                </Form>
+            </Container>
+        );
     }
-  };
-  render() {
-    return (
-      <Container>
-        <Form onSubmit={this.handleRegister}>
-          <Title title="Registro" />
-          {this.state.error && <p>{this.state.error}</p>}
-          <input
-            type="text"
-            placeholder="Primeiro Nome"
-            value={this.state.firstName}
-            onChange={(e) => this.setState({ firstName: e.target.value })}
-          />
-          <input
-            type="text"
-            placeholder="Último Nome"
-            value={this.state.lastName}
-            onChange={(e) => this.setState({ lastName: e.target.value })}
-          />
-          <input
-            type="text"
-            placeholder="Nome de usuário"
-            value={this.state.username}
-            onChange={(e) => this.setState({ username: e.target.value })}
-          />
-          <input
-            type="password"
-            placeholder="Senha"
-            value={this.state.password}
-            onChange={(e) => this.setState({ password: e.target.value })}
-          />
-          <button type="submit">Enviar</button>
-          <hr />
-          <Link to="/">Retornar para a tela de login</Link>
-        </Form>
-      </Container>
-    );
-  }
 }
 
 export default withRouter(SignUp);
