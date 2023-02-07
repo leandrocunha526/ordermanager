4451a7ccd983b693fbd39e1286f0d0fae7397efd
Leandro Cunha <leandrocunha016@gmail.com>
2023-02-07 17:18:57 -0300

    Add MUI alert in employee

--- src/pages/Employee/Employee.js
+++ src/pages/Employee/Employee.js
@@ -3,98 +3,118 @@ import { withRouter } from "react-router-dom";
 import api from "../../services/api";
 import { Form, Container } from "./styles/EmployeeFormStyle";
 import Title from "../../components/title";
-
+import { Alert, AlertTitle } from "@mui/material";
 class Employee extends Component {
-  state = {
-    name: "",
-    salary: "",
-    cpf: "",
-    phone: "",
-    birthday: "",
-    error: "",
-    message: "",
-  };
+    state = {
+        name: "",
+        salary: "",
+        cpf: "",
+        phone: "",
+        birthday: "",
+        error: "",
+        message: "",
+    };
 
-  handleRegister = async (e) => {
-    e.preventDefault();
-    const {name, salary, cpf, phone, birthday} = this.state;
-    if (!name || !salary || !cpf || !phone|| !birthday) {
-      this.setState({
-        error: "Preencha todos os campos para cadastrar um funcionário",
-      });
-    } else {
-      try {
-        await api.post("/api/employees/register", {
-            name,
-            salary,
-            cpf,
-            phone,
-            birthday
-        });
-        this.setState({
-          message: "Salvo com sucesso"
-        })
-      } catch (err) {
-        this.setState({
-          error: "Ocorreu o seguinte problema com o cadastro. " + err,
-        });
-      }
+    handleRegister = async (e) => {
+        e.preventDefault();
+        const { name, salary, cpf, phone, birthday } = this.state;
+        if (!name || !salary || !cpf || !phone || !birthday) {
+            this.setState({
+                error: "Preencha todos os campos para cadastrar um funcionário.",
+            });
+        } else {
+            try {
+                await api.post("/api/employees/register", {
+                    name,
+                    salary,
+                    cpf,
+                    phone,
+                    birthday,
+                });
+                this.setState({
+                    message: "Foi adicionado um novo funcionário!",
+                });
+            } catch (err) {
+                this.setState({
+                    error: "Ocorreu o seguinte problema com o cadastro. " + err,
+                });
+            }
+        }
+    };
+    render() {
+        return (
+            <main>
+                <Container>
+                    <Form onSubmit={this.handleRegister}>
+                        <Title title="Registro de funcionário" />
+                        {this.state.message && (
+                            <Alert severity="success">
+                                <AlertTitle>Sucesso!</AlertTitle>
+                                {this.state.message}
+                            </Alert>
+                        )}
+                        {this.state.error && (
+                            <Alert severity="error">
+                                <AlertTitle>Erro!</AlertTitle>
+                                {this.state.error}
+                            </Alert>
+                        )}
+
+                        <label>Nome</label>
+                        <input
+                            type="text"
+                            placeholder="Nome"
+                            value={this.state.name}
+                            onChange={(e) =>
+                                this.setState({ name: e.target.value })
+                            }
+                        />
+
+                        <label>Salário</label>
+                        <input
+                            type="number"
+                            placeholder="Salário"
+                            value={this.state.salary}
+                            onChange={(e) =>
+                                this.setState({ salary: e.target.value })
+                            }
+                        />
+
+                        <label>CPF</label>
+                        <input
+                            type="text"
+                            placeholder="CPF"
+                            value={this.state.cpf}
+                            onChange={(e) =>
+                                this.setState({ cpf: e.target.value })
+                            }
+                        />
+
+                        <label>Telefone</label>
+                        <input
+                            type="text"
+                            placeholder="Telefone"
+                            value={this.state.phone}
+                            onChange={(e) =>
+                                this.setState({ phone: e.target.value })
+                            }
+                        />
+
+                        <label>Data de nascimento</label>
+                        <input
+                            type="date"
+                            placeholder="Data de nascimento"
+                            value={this.state.birthday}
+                            onChange={(e) =>
+                                this.setState({ birthday: e.target.value })
+                            }
+                        />
+                        <button type="submit">Enviar</button>
+                    </Form>
+                </Container>
+            </main>
+        );
     }
-  };
-  render() {
-    return (
-    <main>
-      <Container>
-        <Form onSubmit={this.handleRegister}>
-          <Title title="Registro de funcionário" />
-          {this.state.message && <p>{this.state.message}</p>}
-          {this.state.error && <p>{this.state.error}</p>}
-
-          <label>Nome</label>
-          <input
-            type="text"
-            placeholder="Nome"
-            value={this.state.name}
-            onChange={(e) => this.setState({ name: e.target.value })}
-          />
-
-          <label>Salário</label>
-          <input
-            type="number"
-            placeholder="Salário"
-            value={this.state.salary}
-            onChange={(e) => this.setState({ salary: e.target.value })}
-          />
-
-          <label>CPF</label>
-          <input
-            type="text"
-            placeholder="CPF"
-            value={this.state.cpf}
-            onChange={(e) => this.setState({ cpf: e.target.value })}
-          />
-
-          <label>Telefone</label>
-          <input
-            type="text"
-            placeholder="Telefone"
-            value={this.state.phone}
-            onChange={(e) => this.setState({ phone: e.target.value })}
-          />
-
-          <label>Data de nascimento</label>
-          <input
-            type="date"
-            placeholder="Data de nascimento"
-            value={this.state.birthday}
-            onChange={(e) => this.setState({ birthday: e.target.value })}
-          />
-          <button type="submit">Enviar</button>
-        </Form>
-      </Container>
-      </main>
-    );
-  }
 }
 
 export default withRouter(Employee);
--- src/pages/Employee/EmployeeTable.js
+++ src/pages/Employee/EmployeeTable.js
@@ -2,94 +2,114 @@ import React, { Component } from "react";
 import api from "../../services/api";
 import { withRouter, Link } from "react-router-dom";
 import "./styles/EmployeeTable.css";
-import Moment from 'react-moment';
+import Moment from "react-moment";
+import { Alert, AlertTitle } from "@mui/material";
 
 class EmployeerTable extends Component {
-  state = {
-    employees: [],
-    message: "",
-    error: ""
-  };
-  componentDidMount() {
-    api
-      .get("api/employees/list")
-      .then((res) => {
-        const employees = res.data;
-        this.setState({ employees });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-  async delete(id) {
-    try {
-      await api.delete(`/api/employees/${id}`);
-      this.setState({
-        message: "Funcionário excluído com sucesso"
-      });
-    } catch (err) {
-      this.setState({
-        error: "Ocorreu o seguinte erro ao excluir o funcionário. " + err,
-      });
+    state = {
+        employees: [],
+        message: "",
+        error: "",
     };
-  };
+    componentDidMount() {
+        api.get("api/employees/list")
+            .then((res) => {
+                const employees = res.data;
+                this.setState({ employees });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+    async delete(id) {
+        try {
+            await api.delete(`/api/employees/${id}`);
+            this.setState({
+                message: "Funcionário " + id + " foi excluído",
+            });
+        } catch (err) {
+            this.setState({
+                error:
+                    "Ocorreu o seguinte erro ao excluir o funcionário " +
+                    id +
+                    ": " +
+                    err,
+            });
+        }
+    }
 
-  render() {
-    return (
-      <main>
-        <div className="usertable__container">
-          <div>
-            <h1>Funcionários cadastrados</h1>
-          </div>
-          {this.state.message && <h3>{this.state.message}</h3>}
-          {this.state.error && <h3>{this.state.error}</h3>}
-          <table>
-            <thead>
-              <tr>
-                <th>Id</th>
-                <th>Nome</th>
-                <th>Data de nascimento</th>
-                <th>Salário</th>
-                <th>CPF</th>
-                <th>Telefone</th>
-                <th>Deletar</th>
-                <th>Editar</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.state.employees.map((employee, index) => (
-                <tr key={index}>
-                  <td>{employee.id}</td>
-                  <td>{employee.name}</td>
-                  <td><Moment format="DD/MM/YYYY">{employee.birthday}</Moment></td>
-                  <td>{employee.salary}</td>
-                  <td>{employee.cpf}</td>
-                  <td>{employee.phone}</td>
-                  <td>
-                    <button
-                      type="button"
-                      className="button__warning"
-                      onClick={() => this.delete(employee.id)}
-                    >
-                      Deletar
-                    </button>
-                  </td>
-                  <td>
-                    <Link
-                      to={"/employee/edit/" + employee.id}
-                      className="button__primary"
-                    >
-                      Editar
-                    </Link>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </main>
-    );
-  }
+    render() {
+        return (
+            <main>
+                <div className="usertable__container">
+                    <div>
+                        <h1>Funcionários cadastrados</h1>
+                    </div>
+                    {this.state.message && (
+                        <Alert severity="success">
+                            <AlertTitle>Sucesso!</AlertTitle>
+                            {this.state.message}
+                        </Alert>
+                    )}
+                    {this.state.error && (
+                        <Alert severity="error">
+                            <AlertTitle>Erro!</AlertTitle>
+                            {this.state.error}
+                        </Alert>
+                    )}
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Id</th>
+                                <th>Nome</th>
+                                <th>Data de nascimento</th>
+                                <th>Salário</th>
+                                <th>CPF</th>
+                                <th>Telefone</th>
+                                <th>Deletar</th>
+                                <th>Editar</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.state.employees.map((employee, index) => (
+                                <tr key={index}>
+                                    <td>{employee.id}</td>
+                                    <td>{employee.name}</td>
+                                    <td>
+                                        <Moment format="DD/MM/YYYY">
+                                            {employee.birthday}
+                                        </Moment>
+                                    </td>
+                                    <td>{employee.salary}</td>
+                                    <td>{employee.cpf}</td>
+                                    <td>{employee.phone}</td>
+                                    <td>
+                                        <button
+                                            type="button"
+                                            className="button__warning"
+                                            onClick={() =>
+                                                this.delete(employee.id)
+                                            }
+                                        >
+                                            Deletar
+                                        </button>
+                                    </td>
+                                    <td>
+                                        <Link
+                                            to={"/employee/edit/" + employee.id}
+                                            className="button__primary"
+                                        >
+                                            Editar
+                                        </Link>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </main>
+        );
+    }
 }
 
 export default withRouter(EmployeerTable);
