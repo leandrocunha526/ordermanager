61d484fd5e519881cfff96a81a042b1928ef7b6b
Leandro Cunha <leandrocunha016@gmail.com>
2023-02-08 01:29:43 -0300

    Add MUI alert in machine

--- src/pages/Machine/Machine.js
+++ src/pages/Machine/Machine.js
@@ -3,145 +3,190 @@ import { withRouter } from "react-router-dom";
 import api from "./../../services/api";
 import Title from "../../components/title";
 import { Form, Container } from "./styles/MachineFormStyle";
+import { Alert, AlertTitle } from "@mui/material";
 
 class Machine extends Component {
-  state = {
-    serialNumber: "",
-    registerCode: "",
-    type: "",
-    implement: "",
-    year: "",
-    acquisitionDate: "",
-    modelMachineId: "",
-    modelsmachine: [],
-    error: "",
-    message: "",
-  };
+    state = {
+        serialNumber: "",
+        registerCode: "",
+        type: "",
+        implement: "",
+        year: "",
+        acquisitionDate: "",
+        modelMachineId: "",
+        modelsmachine: [],
+        error: "",
+        message: "",
+    };
 
-  componentDidMount() {
-    api
-      .get("api/modelsmachine/list")
-      .then((res) => {
-        const modelsmachine = res.data;
-        this.setState({ modelsmachine });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-
-  handleRegister = async (e) => {
-    e.preventDefault();
-    const { registerCode, serialNumber, type, implement, year, acquisitionDate, modelMachineId } =
-      this.state;
-    if (!registerCode || !serialNumber || !type || !implement || !year || !acquisitionDate || !modelMachineId) {
-      this.setState({
-        error: "Preencha todos os campo para cadastrar uma máquina",
-      });
-    } else {
-      try {
-        await api.post("/api/machines/register", {
-          registerCode,
-          serialNumber,
-          type,
-          implement,
-          year,
-          acquisitionDate,
-          modelMachineId,
-        });
-        this.setState({
-          message: "Salvo com sucesso",
-        });
-      } catch (err) {
-        this.setState({
-          error: "Ocorreu o seguinte problema com o cadastro: " + err,
-        });
-      }
+    componentDidMount() {
+        api.get("api/modelsmachine/list")
+            .then((res) => {
+                const modelsmachine = res.data;
+                this.setState({ modelsmachine });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     }
-  };
 
-  render() {
-    return (
-      <main>
-        <Container>
-          <Form onSubmit={this.handleRegister}>
-            <Title title="Cadastro de máquina" />
-            {this.state.message && <p>{this.state.message}</p>}
-            {this.state.error && <p>{this.state.error}</p>}
+    handleRegister = async (e) => {
+        e.preventDefault();
+        const {
+            registerCode,
+            serialNumber,
+            type,
+            implement,
+            year,
+            acquisitionDate,
+            modelMachineId,
+        } = this.state;
+        if (
+            !registerCode ||
+            !serialNumber ||
+            !type ||
+            !implement ||
+            !year ||
+            !acquisitionDate ||
+            !modelMachineId
+        ) {
+            this.setState({
+                error: "Preencha todos os campos para cadastrar uma máquina.",
+            });
+        } else {
+            try {
+                await api.post("/api/machines/register", {
+                    registerCode,
+                    serialNumber,
+                    type,
+                    implement,
+                    year,
+                    acquisitionDate,
+                    modelMachineId,
+                });
+                this.setState({
+                    message: "Salvo com sucesso.",
+                });
+            } catch (err) {
+                this.setState({
+                    error:
+                        "Ocorreu o seguinte problema com o cadastro: " +
+                        err +
+                        ".",
+                });
+            }
+        }
+    };
 
-            <label>Código de registro (Id Agro)</label>
-            <input
-              type="text"
-              placeholder="Código de registro (Id Agro)"
-              value={this.state.registerCode}
-              onChange={(e) => this.setState({ registerCode: e.target.value })}
-            />
+    render() {
+        return (
+            <main>
+                <Container>
+                    <Form onSubmit={this.handleRegister}>
+                        <Title title="Cadastro de máquina" />
+                        {this.state.message && (
+                            <Alert severity="success">
+                                <AlertTitle>Sucesso!</AlertTitle>
+                                {this.state.message}
+                            </Alert>
+                        )}
+                        {this.state.error && (
+                            <Alert severity="error">
+                                <AlertTitle>Erro!</AlertTitle>
+                                {this.state.error}
+                            </Alert>
+                        )}
 
-            <label>Número de serial (chassi)</label>
-            <input
-              type="text"
-              placeholder="Número de serial (chassi)"
-              value={this.state.serialNumber}
-              onChange={(e) => this.setState({ serialNumber: e.target.value })}
-            />
+                        <label>Código de registro (Id Agro)</label>
+                        <input
+                            type="text"
+                            placeholder="Código de registro (Id Agro)"
+                            value={this.state.registerCode}
+                            onChange={(e) =>
+                                this.setState({ registerCode: e.target.value })
+                            }
+                        />
 
-            <label>Tipo de máquina</label>
-            <input
-              type="text"
-              placeholder="Tipo de máquina"
-              value={this.state.type}
-              onChange={(e) => this.setState({ type: e.target.value })}
-            />
+                        <label>Número de serial (chassi)</label>
+                        <input
+                            type="text"
+                            placeholder="Número de serial (chassi)"
+                            value={this.state.serialNumber}
+                            onChange={(e) =>
+                                this.setState({ serialNumber: e.target.value })
+                            }
+                        />
 
-            <label>Implemento da máquina</label>
-            <input
-              type="text"
-              placeholder="Implemento da máquina"
-              value={this.state.implement}
-              onChange={(e) => this.setState({ implement: e.target.value })}
-            />
+                        <label>Tipo de máquina</label>
+                        <input
+                            type="text"
+                            placeholder="Tipo de máquina"
+                            value={this.state.type}
+                            onChange={(e) =>
+                                this.setState({ type: e.target.value })
+                            }
+                        />
 
-            <label>Ano da máquina</label>
-            <input
-              type="number"
-              placeholder="Ano da máquina"
-              value={this.state.year}
-              onChange={(e) => this.setState({ year: e.target.value })}
-            />
+                        <label>Implemento da máquina</label>
+                        <input
+                            type="text"
+                            placeholder="Implemento da máquina"
+                            value={this.state.implement}
+                            onChange={(e) =>
+                                this.setState({ implement: e.target.value })
+                            }
+                        />
 
-            <label>Data de aquisição</label>
-            <input
-              type="date"
-              placeholder="Data de aquisição"
-              value={this.state.acquisitionDate}
-              onChange={(e) =>
-                this.setState({ acquisitionDate: e.target.value })
-              }
-            />
+                        <label>Ano da máquina</label>
+                        <input
+                            type="number"
+                            placeholder="Ano da máquina"
+                            value={this.state.year}
+                            onChange={(e) =>
+                                this.setState({ year: e.target.value })
+                            }
+                        />
 
-            <label>Modelo</label>
-            <select
-              name="modelMachineId"
-              id="modelMachineId"
-              value={this.state.modelMachineId}
-              onChange={(e) =>
-                this.setState({ modelMachineId: e.target.value })
-              }
-            >
-              <option value="0">Selecione um modelo</option>
+                        <label>Data de aquisição</label>
+                        <input
+                            type="date"
+                            placeholder="Data de aquisição"
+                            value={this.state.acquisitionDate}
+                            onChange={(e) =>
+                                this.setState({
+                                    acquisitionDate: e.target.value,
+                                })
+                            }
+                        />
 
-              {this.state.modelsmachine.map((modelmachine) => (
-                <option key={modelmachine.id} value={modelmachine.id}>
-                  {modelmachine.description}
-                </option>
-              ))}
-            </select>
-            <button type="submit">Enviar</button>
-          </Form>
-        </Container>
-      </main>
-    );
-  }
+                        <label>Modelo</label>
+                        <select
+                            name="modelMachineId"
+                            id="modelMachineId"
+                            value={this.state.modelMachineId}
+                            onChange={(e) =>
+                                this.setState({
+                                    modelMachineId: e.target.value,
+                                })
+                            }
+                        >
+                            <option value="0">Selecione um modelo</option>
+
+                            {this.state.modelsmachine.map((modelmachine) => (
+                                <option
+                                    key={modelmachine.id}
+                                    value={modelmachine.id}
+                                >
+                                    {modelmachine.description}
+                                </option>
+                            ))}
+                        </select>
+                        <button type="submit">Enviar</button>
+                    </Form>
+                </Container>
+            </main>
+        );
+    }
 }
 
 export default withRouter(Machine);
--- src/pages/Machine/MachineTable.js
+++ src/pages/Machine/MachineTable.js
@@ -3,99 +3,116 @@ import api from "../../services/api";
 import { withRouter, Link } from "react-router-dom";
 import "./styles/MachineTable.css";
 import Moment from "react-moment";
+import { Alert, AlertTitle } from "@mui/material";
 
 class MachineTable extends Component {
-  state = {
-    machines: [],
-    message: "",
-    error: "",
-  };
-  componentDidMount() {
-    api
-      .get("/api/machines/list")
-      .then((res) => {
-        const machines = res.data;
-        this.setState({ machines });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-  async delete(id) {
-    try {
-      await api.delete(`/api/machines/${id}`);
-      this.setState({
-        message: "Máquina excluída com sucesso",
-      });
-    } catch (err) {
-      this.setState({
-        error: "Ocorreu o seguinte problema ao excluir a máquina: " + err,
-      });
+    state = {
+        machines: [],
+        message: "",
+        error: "",
+    };
+    componentDidMount() {
+        api.get("/api/machines/list")
+            .then((res) => {
+                const machines = res.data;
+                this.setState({ machines });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     }
-  }
+    async delete(id) {
+        try {
+            await api.delete(`/api/machines/${id}`);
+            this.setState({
+                message: "Máquina " + id + " excluída com sucesso.",
+            });
+        } catch (err) {
+            this.setState({
+                error:
+                    "Ocorreu o seguinte problema ao excluir a máquina " +
+                    id +
+                    ": " +
+                    err +
+                    ".",
+            });
+        }
+    }
 
-  render() {
-    return (
-      <main>
-        <div className="usertable__container">
-          <div>
-            <h1>Máquinas cadastradas</h1>
-          </div>
-          {this.state.message && <h3>{this.state.message}</h3>}
-          {this.state.error && <h3>{this.state.error}</h3>}
-          <table>
-            <thead>
-              <tr>
-                <th>Id</th>
-                <th>Código de registro</th>
-                <th>Número de serial</th>
-                <th>Tipo</th>
-                <th>Data de aquisição</th>
-                <th>Modelo</th>
-                <th>Ano</th>
-                <th>Deletar</th>
-                <th>Editar</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.state.machines.map((machine, index) => (
-                <tr key={index}>
-                  <td>{machine.id}</td>
-                  <td>{machine.registerCode}</td>
-                  <td>{machine.serialNumber}</td>
-                  <td>{machine.type}</td>
-                  <td>
-                    <Moment format="DD/MM/YYYY">
-                      {machine.acquisitionDate}
-                    </Moment>
-                  </td>
-                  <td>{machine.modelsMachine.description}</td>
-                  <td>{machine.year}</td>
-                  <td>
-                    <button
-                      type="button"
-                      className="button__warning"
-                      onClick={() => this.delete(machine.id)}
-                    >
-                      Deletar
-                    </button>
-                  </td>
-                  <td>
-                    <Link
-                      to={"/machine/edit/" + machine.id}
-                      className="button__primary"
-                    >
-                      Editar
-                    </Link>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </main>
-    );
-  }
+    render() {
+        return (
+            <main>
+                <div className="usertable__container">
+                    <div>
+                        <h1>Máquinas cadastradas</h1>
+                    </div>
+                    {this.state.message && (
+                        <Alert severity="success">
+                            <AlertTitle>Sucesso!</AlertTitle>
+                            {this.state.message}
+                        </Alert>
+                    )}
+                    {this.state.error && (
+                        <Alert severity="error">
+                            <AlertTitle>Erro!</AlertTitle>
+                            {this.state.error}
+                        </Alert>
+                    )}
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Id</th>
+                                <th>Código de registro</th>
+                                <th>Número de serial</th>
+                                <th>Tipo</th>
+                                <th>Data de aquisição</th>
+                                <th>Modelo</th>
+                                <th>Ano</th>
+                                <th>Deletar</th>
+                                <th>Editar</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.state.machines.map((machine, index) => (
+                                <tr key={index}>
+                                    <td>{machine.id}</td>
+                                    <td>{machine.registerCode}</td>
+                                    <td>{machine.serialNumber}</td>
+                                    <td>{machine.type}</td>
+                                    <td>
+                                        <Moment format="DD/MM/YYYY">
+                                            {machine.acquisitionDate}
+                                        </Moment>
+                                    </td>
+                                    <td>{machine.modelsMachine.description}</td>
+                                    <td>{machine.year}</td>
+                                    <td>
+                                        <button
+                                            type="button"
+                                            className="button__warning"
+                                            onClick={() =>
+                                                this.delete(machine.id)
+                                            }
+                                        >
+                                            Deletar
+                                        </button>
+                                    </td>
+                                    <td>
+                                        <Link
+                                            to={"/machine/edit/" + machine.id}
+                                            className="button__primary"
+                                        >
+                                            Editar
+                                        </Link>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </main>
+        );
+    }
 }
 
 export default withRouter(MachineTable);
