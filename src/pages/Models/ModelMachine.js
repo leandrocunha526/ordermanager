439a458360b97c0a27198ced532f10d36a4fc894
Leandro Cunha <leandrocunha016@gmail.com>
2023-02-08 02:11:22 -0300

    Add MUI alert in machine model

--- src/pages/Models/ModelMachine.js
+++ src/pages/Models/ModelMachine.js
@@ -1,8 +1,9 @@
-import React, {Component} from "react";
-import {withRouter} from "react-router-dom";
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
 import api from "../../services/api";
-import {Form, Container} from "./styles/ModelMachineForm";
+import { Form, Container } from "./styles/ModelMachineForm";
 import Title from "../../components/title";
+import { Alert, AlertTitle } from "@mui/material";
 
 class ModelMachine extends Component {
     state = {
@@ -10,81 +11,99 @@ class ModelMachine extends Component {
         manufacturerId: "",
         manufacturers: [],
         error: "",
-        messsage: ""
-    }
+        message: "",
+    };
 
     componentDidMount() {
-        api
-          .get("api/manufacturers/list")
-          .then((res) => {
-            const manufacturers = res.data;
-            this.setState({ manufacturers });
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      }
+        api.get("api/manufacturers/list")
+            .then((res) => {
+                const manufacturers = res.data;
+                this.setState({ manufacturers });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
 
     handleRegister = async (e) => {
         e.preventDefault();
-        const {description, manufacturerId} = this.state;
+        const { description, manufacturerId } = this.state;
         if (!description || !manufacturerId) {
-          this.setState({
-            error: "Preencha todos os campo para cadastrar um modelo",
-          });
+            this.setState({
+                error: "Preencha todos os campos para cadastrar um modelo.",
+            });
         } else {
-          try {
-            await api.post("/api/modelsmachine/register", {
-                description,
-                manufacturerId,
-            });
-            this.setState({
-              message: "Salvo com sucesso"
-            })
-          } catch (err) {
-            this.setState({
-              error: "Ocorreu o seguinte problema com o cadastro. " + err,
-            });
-          }
+            try {
+                await api.post("/api/modelsmachine/register", {
+                    description,
+                    manufacturerId,
+                });
+                this.setState({
+                    message: "Salvo com sucesso.",
+                });
+            } catch (err) {
+                this.setState({
+                    error: "Ocorreu o seguinte problema com o cadastro. " + err,
+                });
+            }
         }
-      };
+    };
 
-      render() {
+    render() {
         return (
-        <main>
-          <Container>
-            <Form onSubmit={this.handleRegister}>
-              <Title title="Registro de modelo de máquina agrícola" />
-              {this.state.message && <p>{this.state.message}</p>}
-              {this.state.error && <p>{this.state.error}</p>}
-              <label>Nome do modelo</label>
-              <input
-                type="text"
-                placeholder="Nome do modelo"
-                value={this.state.description}
-                onChange={(e) => this.setState({ description: e.target.value })}
-              />
+            <main>
+                <Container>
+                    <Form onSubmit={this.handleRegister}>
+                        <Title title="Registro de modelo de máquina agrícola" />
+                        {this.state.message && (
+                            <Alert severity="success">
+                                <AlertTitle>Sucesso!</AlertTitle>
+                                {this.state.message}
+                            </Alert>
+                        )}
+                        {this.state.error && (
+                            <Alert severity="error">
+                                <AlertTitle>Erro!</AlertTitle>
+                                {this.state.error}
+                            </Alert>
+                        )}
+                        <label>Nome do modelo</label>
+                        <input
+                            type="text"
+                            placeholder="Nome do modelo"
+                            value={this.state.description}
+                            onChange={(e) =>
+                                this.setState({ description: e.target.value })
+                            }
+                        />
 
-              <label>Fabricante</label>
-              <select
-                name="manufacturerId"
-                id="manufacturerId"
-                value={this.state.manufacturerId}
-                onChange={(e) => this.setState({ manufacturerId: e.target.value })}
-              >
-                <option value="0">Selecione um fabricante</option>
+                        <label>Fabricante</label>
+                        <select
+                            name="manufacturerId"
+                            id="manufacturerId"
+                            value={this.state.manufacturerId}
+                            onChange={(e) =>
+                                this.setState({
+                                    manufacturerId: e.target.value,
+                                })
+                            }
+                        >
+                            <option value="0">Selecione um fabricante</option>
 
-                {this.state.manufacturers.map((manufacturers) => (
-                  <option key={manufacturers.id} value={manufacturers.id}>
-                    {manufacturers.description}
-                  </option>
-                ))}
-              </select>
-              <button type="submit">Enviar</button>
-            </Form>
-          </Container>
-          </main>
+                            {this.state.manufacturers.map((manufacturers) => (
+                                <option
+                                    key={manufacturers.id}
+                                    value={manufacturers.id}
+                                >
+                                    {manufacturers.description}
+                                </option>
+                            ))}
+                        </select>
+                        <button type="submit">Enviar</button>
+                    </Form>
+                </Container>
+            </main>
         );
-      }
+    }
 }
 export default withRouter(ModelMachine);
--- src/pages/Models/ModelMachineTable.js
+++ src/pages/Models/ModelMachineTable.js
@@ -2,87 +2,116 @@ import React, { Component } from "react";
 import api from "../../services/api";
 import { withRouter, Link } from "react-router-dom";
 import "./styles/ModelMachineTable.css";
+import { Alert, AlertTitle } from "@mui/material";
 
 class ModelMachineTable extends Component {
-  state = {
-    modelsmachine: [],
-    message: "",
-    error: ""
-  };
-  componentDidMount() {
-    api
-      .get("api/modelsmachine/list")
-      .then((res) => {
-        const modelsmachine = res.data;
-        this.setState({ modelsmachine });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-  async delete(id) {
-    try {
-      await api.delete(`/api/modelsmachine/${id}`);
-      this.setState({
-        message: "Modelo excluído com sucesso"
-      });
-    } catch (err) {
-      this.setState({
-        error: "Ocorreu o seguinte erro ao excluir o modelo: " + err,
-      });
+    state = {
+        modelsmachine: [],
+        message: "",
+        error: "",
     };
-  };
+    componentDidMount() {
+        api.get("api/modelsmachine/list")
+            .then((res) => {
+                const modelsmachine = res.data;
+                this.setState({ modelsmachine });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+    async delete(id) {
+        try {
+            await api.delete(`/api/modelsmachine/${id}`);
+            this.setState({
+                message: "Modelo " + id + " excluído com sucesso.",
+            });
+        } catch (err) {
+            this.setState({
+                error:
+                    "Ocorreu o seguinte erro ao excluir o modelo " +
+                    id +
+                    ": " +
+                    err +
+                    ".",
+            });
+        }
+    }
 
-  render() {
-    return (
-      <main>
-        <div className="usertable__container">
-          <div>
-            <h1>Modelos cadastrados</h1>
-          </div>
-          {this.state.message && <h3>{this.state.message}</h3>}
-          {this.state.error && <h3>{this.state.error}</h3>}
-          <table>
-            <thead>
-              <tr>
-                <th>Id</th>
-                <th>Nome do modelo</th>
-                <th>Fabricante</th>
-                <th>Deletar</th>
-                <th>Editar</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.state.modelsmachine.map((modelsmachine, index) => (
-                <tr key={index}>
-                  <td>{modelsmachine.id}</td>
-                  <td>{modelsmachine.description}</td>
-                  <td>{modelsmachine.manufacturers.description}</td>
-                  <td>
-                    <button
-                      type="button"
-                      className="button__warning"
-                      onClick={() => this.delete(modelsmachine.id)}
-                    >
-                      Deletar
-                    </button>
-                  </td>
-                  <td>
-                    <Link
-                      to={"/model/edit/" + modelsmachine.id}
-                      className="button__primary"
-                    >
-                      Editar
-                    </Link>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </main>
-    );
-  }
+    render() {
+        return (
+            <main>
+                <div className="usertable__container">
+                    <div>
+                        <h1>Modelos cadastrados</h1>
+                    </div>
+                    {this.state.message && (
+                        <Alert severity="success">
+                            <AlertTitle>Sucesso!</AlertTitle>
+                            {this.state.message}
+                        </Alert>
+                    )}
+                    {this.state.error && (
+                        <Alert severity="error">
+                            <AlertTitle>Erro!</AlertTitle>
+                            {this.state.error}
+                        </Alert>
+                    )}
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Id</th>
+                                <th>Nome do modelo</th>
+                                <th>Fabricante</th>
+                                <th>Deletar</th>
+                                <th>Editar</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.state.modelsmachine.map(
+                                (modelsmachine, index) => (
+                                    <tr key={index}>
+                                        <td>{modelsmachine.id}</td>
+                                        <td>{modelsmachine.description}</td>
+                                        <td>
+                                            {
+                                                modelsmachine.manufacturers
+                                                    .description
+                                            }
+                                        </td>
+                                        <td>
+                                            <button
+                                                type="button"
+                                                className="button__warning"
+                                                onClick={() =>
+                                                    this.delete(
+                                                        modelsmachine.id
+                                                    )
+                                                }
+                                            >
+                                                Deletar
+                                            </button>
+                                        </td>
+                                        <td>
+                                            <Link
+                                                to={
+                                                    "/model/edit/" +
+                                                    modelsmachine.id
+                                                }
+                                                className="button__primary"
+                                            >
+                                                Editar
+                                            </Link>
+                                        </td>
+                                    </tr>
+                                )
+                            )}
+                        </tbody>
+                    </table>
+                </div>
+            </main>
+        );
+    }
 }
 
 export default withRouter(ModelMachineTable);
