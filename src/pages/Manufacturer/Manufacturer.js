3173563783981d2035eb991c99ca8d2e6304adb7
Leandro Cunha <leandrocunha016@gmail.com>
2023-02-08 01:57:18 -0300

    Add MUI alert in manufacturer

--- src/pages/Manufacturer/Manufacturer.js
+++ src/pages/Manufacturer/Manufacturer.js
@@ -3,57 +3,69 @@ import { withRouter } from "react-router-dom";
 import api from "../../services/api";
 import { Form, Container } from "./styles/ManufacturerFormStyle";
 import Title from "../../components/title";
-
+import { Alert, AlertTitle } from "@mui/material";
 class Manufacturer extends Component {
-  state = {
-    description: "",
-    error: "",
-    message: "",
-  };
+    state = {
+        description: "",
+        error: "",
+        message: "",
+    };
 
-  handleRegister = async (e) => {
-    e.preventDefault();
-    const {description} = this.state;
-    if (!description) {
-      this.setState({
-        error: "Preencha o campo para cadastrar uma marca",
-      });
-    } else {
-      try {
-        await api.post("/api/manufacturers/register", {
-            description,
-        });
-        this.setState({
-          message: "Salvo com sucesso"
-        })
-      } catch (err) {
-        this.setState({
-          error: "Ocorreu o seguinte problema com o cadastro. " + err,
-        });
-      }
+    handleRegister = async (e) => {
+        e.preventDefault();
+        const { description } = this.state;
+        if (!description) {
+            this.setState({
+                error: "Preencha o campo para cadastrar uma marca.",
+            });
+        } else {
+            try {
+                await api.post("/api/manufacturers/register", {
+                    description,
+                });
+                this.setState({
+                    message: "Salvo com sucesso.",
+                });
+            } catch (err) {
+                this.setState({
+                    error: "Ocorreu o seguinte problema com o cadastro. " + err,
+                });
+            }
+        }
+    };
+    render() {
+        return (
+            <main>
+                <Container>
+                    <Form onSubmit={this.handleRegister}>
+                        <Title title="Registro de marca" />
+                        {this.state.message && (
+                            <Alert severity="success">
+                                <AlertTitle>Sucesso!</AlertTitle>
+                                {this.state.message}
+                            </Alert>
+                        )}
+                        {this.state.error && (
+                            <Alert severity="error">
+                                <AlertTitle>Erro!</AlertTitle>
+                                {this.state.error}
+                            </Alert>
+                        )}
+                        <label>Nome da marca</label>
+                        <input
+                            type="text"
+                            placeholder="Nome da marca"
+                            value={this.state.description}
+                            onChange={(e) =>
+                                this.setState({ description: e.target.value })
+                            }
+                        />
+                        <button type="submit">Enviar</button>
+                    </Form>
+                </Container>
+            </main>
+        );
     }
-  };
-  render() {
-    return (
-    <main>
-      <Container>
-        <Form onSubmit={this.handleRegister}>
-          <Title title="Registro de marca" />
-          {this.state.message && <p>{this.state.message}</p>}
-          {this.state.error && <p>{this.state.error}</p>}
-          <label>Nome da marca</label>
-          <input
-            type="text"
-            placeholder="Nome da marca"
-            value={this.state.description}
-            onChange={(e) => this.setState({ description: e.target.value })}
-          />
-          <button type="submit">Enviar</button>
-        </Form>
-      </Container>
-      </main>
-    );
-  }
 }
 
 export default withRouter(Manufacturer);
--- src/pages/Manufacturer/ManufacturerTable.js
+++ src/pages/Manufacturer/ManufacturerTable.js
@@ -2,85 +2,107 @@ import React, { Component } from "react";
 import api from "../../services/api";
 import { withRouter, Link } from "react-router-dom";
 import "./styles/ManufacturerTable.css";
+import { Alert, AlertTitle } from "@mui/material";
 
 class ManufacturerTable extends Component {
-  state = {
-    manufacturers: [],
-    message: "",
-    error: ""
-  };
-  componentDidMount() {
-    api
-      .get("api/manufacturers/list")
-      .then((res) => {
-        const manufacturers = res.data;
-        this.setState({ manufacturers });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-  async delete(id) {
-    try {
-      await api.delete(`/api/manufacturers/${id}`);
-      this.setState({
-        message: "Marca excluída com sucesso"
-      });
-    } catch (err) {
-      this.setState({
-        error: "Ocorreu o seguinte erro ao excluir a marca. " + err,
-      });
+    state = {
+        manufacturers: [],
+        message: "",
+        error: "",
     };
-  };
+    componentDidMount() {
+        api.get("api/manufacturers/list")
+            .then((res) => {
+                const manufacturers = res.data;
+                this.setState({ manufacturers });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+    async delete(id) {
+        try {
+            await api.delete(`/api/manufacturers/${id}`);
+            this.setState({
+                message: "Marca " + id + " excluída com sucesso.",
+            });
+        } catch (err) {
+            this.setState({
+                error:
+                    "Ocorreu o seguinte erro ao excluir a marca " +
+                    id +
+                    ": " +
+                    err +
+                    ".",
+            });
+        }
+    }
 
-  render() {
-    return (
-      <main>
-        <div className="usertable__container">
-          <div>
-            <h1>Marcas cadastradas</h1>
-          </div>
-          {this.state.message && <h3>{this.state.message}</h3>}
-          {this.state.error && <h3>{this.state.error}</h3>}
-          <table>
-            <thead>
-              <tr>
-                <th>Id</th>
-                <th>Nome</th>
-                <th>Deletar</th>
-                <th>Editar</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.state.manufacturers.map((manufacturer, index) => (
-                <tr key={index}>
-                  <td>{manufacturer.id}</td>
-                  <td>{manufacturer.description}</td>
-                  <td>
-                    <button
-                      type="button"
-                      className="button__warning"
-                      onClick={() => this.delete(manufacturer.id)}
-                    >
-                      Deletar
-                    </button>
-                  </td>
-                  <td>
-                    <Link
-                      to={"/manufacturer/edit/" + manufacturer.id}
-                      className="button__primary"
-                    >
-                      Editar
-                    </Link>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </main>
-    );
-  }
+    render() {
+        return (
+            <main>
+                <div className="usertable__container">
+                    <div>
+                        <h1>Marcas cadastradas</h1>
+                    </div>
+                    {this.state.message && (
+                        <Alert severity="success">
+                            <AlertTitle>Sucesso!</AlertTitle>
+                            {this.state.message}
+                        </Alert>
+                    )}
+                    {this.state.error && (
+                        <Alert severity="error">
+                            <AlertTitle>Error!</AlertTitle>
+                            {this.state.error}
+                        </Alert>
+                    )}
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Id</th>
+                                <th>Nome</th>
+                                <th>Deletar</th>
+                                <th>Editar</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.state.manufacturers.map(
+                                (manufacturer, index) => (
+                                    <tr key={index}>
+                                        <td>{manufacturer.id}</td>
+                                        <td>{manufacturer.description}</td>
+                                        <td>
+                                            <button
+                                                type="button"
+                                                className="button__warning"
+                                                onClick={() =>
+                                                    this.delete(manufacturer.id)
+                                                }
+                                            >
+                                                Deletar
+                                            </button>
+                                        </td>
+                                        <td>
+                                            <Link
+                                                to={
+                                                    "/manufacturer/edit/" +
+                                                    manufacturer.id
+                                                }
+                                                className="button__primary"
+                                            >
+                                                Editar
+                                            </Link>
+                                        </td>
+                                    </tr>
+                                )
+                            )}
+                        </tbody>
+                    </table>
+                </div>
+            </main>
+        );
+    }
 }
 
 export default withRouter(ManufacturerTable);
