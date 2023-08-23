8a27ea3ce6c428a8a68a9fdd4f9eccf7d3b5e595
Leandro Cunha <leandrocunha016@gmail.com>
2023-08-23 11:41:23 -0300

    Add useLocation and active link in sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import "./Sidebar.css";
-import { Link, withRouter } from "react-router-dom";
+import { Link, withRouter, useLocation } from "react-router-dom";
 
 const Sidebar = ({ sidebarOpen, closeSidebar }) => {
+    const location = useLocation();
     return (
         <div className={sidebarOpen ? "sidebar__responsive" : ""} id="sidebar">
             <div className="sidebar__title">
@@ -17,59 +18,59 @@ const Sidebar = ({ sidebarOpen, closeSidebar }) => {
             </div>
             <div className="sidebar__menu">
                 <h2>Agendamentos</h2>
-                <div className="sidebar__link">
+                <div className={`sidebar__link ${location.pathname === "/orders/register" ? "active" : ""}`}>
                     <i className="fa fa-calendar"></i>
                     <Link to="/orders/register">Agendar ordem de serviço</Link>
                 </div>
                 <h2>Cadastros</h2>
-                <div className="sidebar__link">
+                <div className={`sidebar__link ${location.pathname === "/manufacturer/register" ? "active" : ""}`}>
                     <i className="fa fa-industry"></i>
                     <Link to="/manufacturer/register">Cadastro de marca</Link>
                 </div>
-                <div className="sidebar__link">
+                <div className={`sidebar__link ${location.pathname === "/models/register" ? "active" : ""}`}>
                     <i className="fas fa-tractor"></i>
                     <Link to="/models/register">Cadastro de modelo</Link>
                 </div>
-                <div className="sidebar__link">
+                <div className={`sidebar__link ${location.pathname === "/machine/register" ? "active" : ""}`}>
                     <i className="fas fa-tractor"></i>
                     <Link to="/machine/register">Cadastro de máquina</Link>
                 </div>
-                <div className="sidebar__link">
+                <div className={`sidebar__link ${location.pathname === "/provider/register" ? "active" : ""}`}>
                     <i className="fa fa-industry"></i>
                     <Link to="/provider/register">Cadastro de fornecedor</Link>
                 </div>
-                <div className="sidebar__link">
+                <div className={`sidebar__link ${location.pathname === "/agriculturalinput/register" ? "active" : ""}`}>
                     <i className="fas fa-seedling"></i>
                     <Link to="/agriculturalinput/register">
                         Cadastro de insumo
                     </Link>
                 </div>
-                <div className="sidebar__link">
+                <div className={`sidebar__link ${location.pathname === "/employee/register" ? "active" : ""}`}>
                     <i className="fas fa-users-cog"></i>
                     <Link to="/employee/register">Cadastro de funcionário</Link>
                 </div>
                 <h2>Listagens</h2>
-                <div className="sidebar__link">
+                <div className={`sidebar__link ${location.pathname === "/manufacturer/list" ? "active" : ""}`}>
                     <i className="fa fa-industry"></i>
                     <Link to="/manufacturer/list">Marcas</Link>
                 </div>
-                <div className="sidebar__link">
+                <div className={`sidebar__link ${location.pathname === "/models" ? "active" : ""}`}>
                     <i className="fas fa-tractor"></i>
                     <Link to="/models">Modelos</Link>
                 </div>
-                <div className="sidebar__link">
+                <div className={`sidebar__link ${location.pathname === "/machine/list" ? "active" : ""}`}>
                     <i className="fas fa-tractor"></i>
                     <Link to="/machine/list">Máquinas</Link>
                 </div>
-                <div className="sidebar__link">
+                <div className={`sidebar__link ${location.pathname === "/provider/list" ? "active" : ""}`}>
                     <i className="fa fa-industry"></i>
                     <Link to="/provider/list">Fornecedores</Link>
                 </div>
-                <div className="sidebar__link">
+                <div className={`sidebar__link ${location.pathname === "/agriculturalinput/list" ? "active" : ""}`}>
                     <i className="fas fa-seedling"></i>
                     <Link to="/agriculturalinput/list">Insumos</Link>
                 </div>
-                <div className="sidebar__link">
+                <div className={`sidebar__link ${location.pathname === "/employee/list" ? "active" : ""}`}>
                     <i className="fas fa-users-cog"></i>
                     <Link to="/employee/list">Funcionários</Link>
                 </div>
